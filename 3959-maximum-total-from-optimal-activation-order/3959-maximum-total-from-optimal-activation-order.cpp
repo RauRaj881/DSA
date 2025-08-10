19b81330293aaefc3bd49ed3953b8b478815f11b
Raushan Raj <2023ugee052@nitjsr.ac.in>
2025-08-10 13:08:22 +0530

    Time: 266 ms (54.48%), Space: 307.6 MB (46.63%) - LeetHub

--- 3959-maximum-total-from-optimal-activation-order/3959-maximum-total-from-optimal-activation-order.cpp
+++ 3959-maximum-total-from-optimal-activation-order/3959-maximum-total-from-optimal-activation-order.cpp
@@ -1,23 +1,21 @@
 class Solution {
-    using ll = long long;
 public:
     long long maxTotal(vector<int>& value, vector<int>& limit) {
-        int n = value.size();
-        unordered_map<int, vector<int>> u;
-        
-        for (int i = 0; i < n; i++) {
-            u[limit[i]].push_back(value[i]);
+        unordered_map<int,vector<int>> mp;
+        int n=value.size();
+        for(int i=0;i<n;i++){
+            mp[limit[i]].push_back(value[i]);
         }
-
-        ll ans = 0;
-        for (int lim = 1; lim <= n; lim++) {
-            if (!u.count(lim)) continue;
-            sort(u[lim].rbegin(), u[lim].rend());
-            for (int i = 0; i < min(lim, (int)u[lim].size()); i++) {
-                ans += u[lim][i];
+        long long ans=0;
+        for(int lim=1;lim<=n;lim++){
+            if(mp.find(lim)!=mp.end()){
+                sort(mp[lim].rbegin(),mp[lim].rend());
+                int size=mp[lim].size();
+                for(int i=0;i<min(lim,size);i++){
+                    ans+=mp[lim][i];
+                }
             }
         }
-
         return ans;
     }
 };
