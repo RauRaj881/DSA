4e4e1e69da601779584de0f8beeb78f1d56d35ca
Raushan Raj <2023ugee052@nitjsr.ac.in>
2025-08-15 16:16:56 +0530

    Time: 0 ms (100%), Space: 9.3 MB (58.07%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,4 +1,5 @@
-class Solution {
+// memoiazation
+/*class Solution {
 public:
 int nofways(int x,int y,vector<vector<int>> &dp){
     if(x==0 && y==0){return 1;}
@@ -11,5 +12,26 @@ int nofways(int x,int y,vector<vector<int>> &dp){
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m,vector<int>(n,-1));
         return nofways(m-1,n-1,dp);
+    }
+};*/
+// tabulation
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        dp[0][0]=1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(!(i==0 && j==0)){
+                    int up,left;
+                    if(j-1<0){up=0;}
+                    else{up=dp[i][j-1];}
+                    if(i-1<0){left=0;}
+                    else{left=dp[i-1][j];}
+                    dp[i][j]=up+left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
