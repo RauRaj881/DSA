2a6427c4fb6b7ce70f61a9fd85107ec586c17ad3
Raushan Raj <2023ugee052@nitjsr.ac.in>
2025-08-15 17:27:51 +0530

    Time: 0 ms (100%), Space: 11.9 MB (28.61%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,4 +1,5 @@
-class Solution {
+//tabulation
+/*class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m=obstacleGrid.size();
@@ -22,5 +23,25 @@ public:
         }
         return dp[m-1][n-1];
         
+    }
+};*/
+// memoiazation
+class Solution {
+public:
+int nofways(int x,int y, vector<vector<int>> &dp,vector<vector<int>>& obstacleGrid){
+    if(x==0&&y==0){return 1;}
+    else if(x<0||y<0){return 0;}
+    if(dp[x][y]!=-1){return dp[x][y];}
+    if(obstacleGrid[x][y]==1){return dp[x][y]=0;}
+    int up=nofways(x-1,y,dp,obstacleGrid);
+    int left=nofways(x,y-1,dp,obstacleGrid);
+    return dp[x][y]=up+left;
+}
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        if(obstacleGrid[0][0]==1){return 0;}
+        int m=obstacleGrid.size();
+        int n=obstacleGrid[0].size();
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return nofways(m-1,n-1,dp,obstacleGrid);
     }
 };
