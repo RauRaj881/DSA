9a75e89784747f377b3bed00c29992f6432e9aaa
Raushan Raj <2023ugee052@nitjsr.ac.in>
2025-08-15 18:04:56 +0530

    Time: 0 ms (100%), Space: 11.5 MB (87.17%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -26,7 +26,7 @@ public:
     }
 };*/
 // memoiazation
-class Solution {
+/*class Solution {
 public:
 int nofways(int x,int y, vector<vector<int>> &dp,vector<vector<int>>& obstacleGrid){
     if(x==0&&y==0){return 1;}
@@ -43,5 +43,28 @@ int nofways(int x,int y, vector<vector<int>> &dp,vector<vector<int>>& obstacleGr
         int n=obstacleGrid[0].size();
         vector<vector<int>> dp(m,vector<int>(n,-1));
         return nofways(m-1,n-1,dp,obstacleGrid);
+    }
+};*/
+// space optimization
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m=obstacleGrid.size();
+        int n=obstacleGrid[0].size();
+        vector<int> prev(n,0);
+        if(obstacleGrid[0][0]==1){return 0;}
+        for(int i=0;i<m;i++){
+             vector<int> curr(n,0);
+            for(int j=0;j<n;j++){
+                if(obstacleGrid[i][j]==1){curr[j]=0;continue;}
+                if(i==0&&j==0){curr[j]=1;continue;}
+                int up=0,left=0;
+                if(i>0){up=prev[j];}
+                if(j>0){left=curr[j-1];}
+                curr[j]=up+left;
+            }
+            prev=curr;
+        }
+        return prev[n-1];  
     }
 };
