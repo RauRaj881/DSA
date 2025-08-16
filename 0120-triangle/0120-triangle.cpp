8109bf18fdc671291996c65cabd67134450bfed9
Raushan Raj <2023ugee052@nitjsr.ac.in>
2025-08-16 11:53:29 +0530

    Time: 0 ms (100%), Space: 13.3 MB (41.12%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,39 @@
+//memoization (not passing all the test cases tle)
+/*class Solution {
+public:
+int mintri(int x,int y,vector<vector<int>> &dp,vector<vector<int>>& triangle,int m){
+    if(y>m-1 ||y>triangle[x].size()-1){return 1e9;}
+    if(dp[x][y]!=-1){return dp[x][y];}
+    if(x==m-1){return triangle[x][y];}
+    int down=triangle[x][y]+mintri(x+1,y,dp,triangle,m);
+    int diagonal=triangle[x][y]+mintri(x+1,y+1,dp,triangle,m);
+    return dp[x][y]=min(down,diagonal);
+}
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int m=triangle.size();
+        vector<vector<int>> dp(m);
+        for(int i=0;i<m;i++){
+            dp[i].resize(triangle[i].size(),-1);
+        }
+        return mintri(0,0,dp,triangle,m);
+    }
+};*/
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int m=triangle.size();
+        if(m==1){return triangle[0][0];}
+        vector<vector<int>> dp(m,vector<int>(m,-1));
+        for(int j=0;j<=m-1;j++){
+            dp[m-1][j]=triangle[m-1][j];
+        }
+        for(int i=m-2;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                int down=triangle[i][j]+dp[i+1][j];
+                int diagonal=triangle[i][j]+dp[i+1][j+1];
+                dp[i][j]=min(down,diagonal);
+            }
+        }
+        return dp[0][0];
+    }
+};
