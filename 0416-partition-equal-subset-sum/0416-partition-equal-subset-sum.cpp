a103cc87a7385a6f092c0849c9fa4d27b6d86d4b
Raushan Raj <2023ugee052@nitjsr.ac.in>
2025-08-20 19:34:02 +0530

    Time: 158 ms (29.93%), Space: 17.4 MB (67.46%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,4 +1,5 @@
-class Solution {
+//memoization
+/*class Solution {
 public:
 bool SubSumUtil(int index,int target,vector<vector<int>> &dp,vector<int>&nums){
     if(target==0){return true;}
@@ -23,5 +24,36 @@ bool SubSumUtil(int index,int target,vector<vector<int>> &dp,vector<int>&nums){
             vector<vector<int>> dp(n,vector<int>(target+1,-1));
             return SubSumUtil(n-1,target,dp,nums);
         } 
+    }
+};*/
+// tabulation
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int n=nums.size();
+        int sum=0;
+        for(auto it:nums){
+            sum+=it;
+        }
+        if(sum%2==1){return false;}
+            int target=sum/2;
+            vector<vector<bool>> dp(n,vector<bool>(target+1,0));
+            for(int i=0;i<n;i++){
+                dp[i][0]=1;
+            }
+            if(nums[0]<=target){
+                dp[0][nums[0]]=1;
+            }
+            for(int i=1;i<n;i++){
+                for(int sum=1;sum<=target;sum++){
+                    bool nottake=dp[i-1][sum];//not pick se
+                    bool take =0;
+                    if(nums[i]<=sum){
+                        take=dp[i-1][sum-nums[i]];//take se
+                    }
+                    dp[i][sum]=take||nottake;
+                }
+            }
+            return dp[n-1][target];
     }
 };
