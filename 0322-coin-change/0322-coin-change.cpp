bc0a14d390fb40037cabc76ee0a3afba54cecd60
Raushan Raj <2023ugee052@nitjsr.ac.in>
2025-08-20 23:00:45 +0530

    Time: 50 ms (27.79%), Space: 48.8 MB (48.08%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,4 +1,5 @@
-class Solution {
+//memoization
+/*class Solution {
 public:
 int coinchange(int index,int amount, vector<vector<int>> &dp,vector<int>& coins){
     if(index==0){
@@ -19,5 +20,31 @@ int coinchange(int index,int amount, vector<vector<int>> &dp,vector<int>& coins)
         int x=coinchange(n-1,amount,dp,coins);
         if(x!=1e9){return x;}
         else return -1;
+    }
+};*/
+//tabulation
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n=coins.size();
+        vector<vector<int>> dp(n,vector<int>(amount+1,0));
+        for(int sum=0;sum<=amount;sum++){
+        if(sum%coins[0]==0){
+            dp[0][sum]=sum/coins[0];
+        }
+        else dp[0][sum]=1e9;
+        }
+        for(int i=1;i<n;i++){
+            for(int sum=0;sum<=amount;sum++){
+                int nottake=dp[i-1][sum];
+                int take=1e9;
+                if(coins[i]<=sum){
+                    take=1+dp[i][sum-coins[i]];
+                }
+                dp[i][sum]=min(take,nottake);
+            }
+        }
+        if(dp[n-1][amount]!=1e9){return dp[n-1][amount];;}
+        return -1;
     }
 };
