cb16c37a6a9df429b6183de4be71ed14d04cf799
Raushan Raj <2023ugee052@nitjsr.ac.in>
2025-08-22 20:57:28 +0530

    Time: 3 ms (87.75%), Space: 15 MB (61.4%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,4 +1,5 @@
-class Solution {
+//memoization
+/*class Solution {
 public:
 int tarsum(int index, vector<vector<int>> &dp,vector<int>& nums,int target){
     if(index==0){
@@ -24,5 +25,71 @@ int tarsum(int index, vector<vector<int>> &dp,vector<int>& nums,int target){
         if ((totsum + target) % 2 != 0 || abs(target) > totsum) return 0;
         vector<vector<int>> dp(n,vector<int>(newtar+1,-1));
         return tarsum(n-1,dp,nums,newtar);
+    }
+};*/
+//tabulation
+/*class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n=nums.size();
+        int totsum=0;
+        for(auto it:nums){
+            totsum+=it;
+        }
+        int newtar=(totsum+target)/2;
+        if ((totsum + target) % 2 != 0 || abs(target) > totsum) return 0;
+        vector<vector<int>> dp(n,vector<int>(newtar+1,0));
+        if (nums[0] == 0) {
+            dp[0][0] = 2;  // either take or not take zero
+        } else {
+            dp[0][0] = 1;  // not take
+            if (nums[0] <= newtar) dp[0][nums[0]] = 1; // take
+        }
+
+        for(int i=1;i<n;i++){
+            for(int sum=0;sum<=newtar;sum++){
+                int nottake=dp[i-1][sum];
+                int take=0;
+                if(nums[i]<=sum){
+                    take=dp[i-1][sum-nums[i]];
+                }
+                dp[i][sum]=take+nottake;
+            }
+        }
+        return dp[n-1][newtar];
+    }
+};*/
+//space optimization
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n=nums.size();
+        int totsum=0;
+        for(auto it:nums){
+            totsum+=it;
+        }
+        int newtar=(totsum+target)/2;
+        if ((totsum + target) % 2 != 0 || abs(target) > totsum) return 0;
+        vector<int> prev(newtar+1,0);
+        if(nums[0]==0){
+            prev[0]=2;//pick+notpick
+        }
+        else{prev[0]=1;}//notpick
+        if(nums[0]!=0&&nums[0]<=newtar){
+            prev[nums[0]]=1;//pick
+        }
+        for(int i=1;i<n;i++){
+            vector<int> curr(newtar+1);
+            for(int sum=0;sum<=newtar;sum++){
+                int nottake=prev[sum];
+                int take=0;
+                if(nums[i]<=sum){
+                    take=prev[sum-nums[i]];
+                }
+                curr[sum]=take+nottake;
+            }
+            prev=curr;
+        }
+        return prev[newtar];
     }
 };
