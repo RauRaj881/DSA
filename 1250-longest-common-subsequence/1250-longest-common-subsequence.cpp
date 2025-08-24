3b9bce01e0d4c5767e3c3fa6e6fdd0b12a15fe8e
Raushan Raj <2023ugee052@nitjsr.ac.in>
2025-08-24 12:50:41 +0530

    Time: 63 ms (5.1%), Space: 27.8 MB (6.19%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,10 +1,11 @@
+//memoization
 class Solution {
 public:
 int locs(int index1,int index2,string &text1,string &text2,vector<vector<int>> &dp){
-    if (index1 < 0 || index2 < 0) return 0;
+    if (index1 == 0 || index2 == 0) return 0;
     int take=0;
     if(dp[index1][index2]!=-1){return dp[index1][index2];}
-    if(text1[index1]==text2[index2]){
+    if(text1[index1-1]==text2[index2-1]){
         take=1+locs(index1-1,index2-1,text1,text2,dp);
     }
     int nottake=max(locs(index1-1,index2,text1,text2,dp),locs(index1,index2-1,text1,text2,dp));
@@ -13,7 +14,21 @@ int locs(int index1,int index2,string &text1,string &text2,vector<vector<int>> &
     int longestCommonSubsequence(string text1, string text2) {
         int n1=text1.size();
         int n2=text2.size();
-        vector<vector<int>> dp(n1,vector<int>(n2,-1));
-        return locs(n1-1,n2-1,text1,text2,dp);      
+        vector<vector<int>> dp(n1+1,vector<int>(n2+1,-1));
+        return locs(n1,n2,text1,text2,dp);      
     }
 };
+//tabulation
+/*class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n1=text1.size();
+        int n2=text2.size();
+        vector<vector<int>> dp(n1,vector<int>(n2,-1));
+        for(int i)
+        
+
+
+        
+    }
+};*/
