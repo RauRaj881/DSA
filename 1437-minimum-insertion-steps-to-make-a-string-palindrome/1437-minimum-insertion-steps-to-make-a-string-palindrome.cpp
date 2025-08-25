24f6a32034f45d25552483e597ed91e0e4fd8b2e
Raushan Raj <2023ugee052@nitjsr.ac.in>
2025-08-25 14:54:00 +0530

    Time: 38 ms (25.78%), Space: 30.6 MB (78.65%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,4 +1,5 @@
-class Solution {
+//tabulation
+/*class Solution {
 public:
     int minInsertions(string s) {
         string t=s;
@@ -37,5 +38,29 @@ public:
             }
         }
         return n-ans.size();
+    }
+};*/
+class Solution {
+public:
+    int minInsertions(string s) {
+        string t=s;
+        reverse(s.begin(),s.end());
+        int n=s.size();
+        int maxno=0;
+        vector<int> prev(n+1,0);
+        for(int i=1;i<=n;i++){
+            vector<int> curr(n+1,0);
+            for(int j=1;j<=n;j++){
+                int nottake=max(prev[j],curr[j-1]);
+                int take=0;
+                if(s[i-1]==t[j-1]){
+                    take=1+prev[j-1];
+                }
+                curr[j]=max(take,nottake);
+                maxno=max(maxno,curr[j]);
+            }
+            prev=curr;
+        }
+        return n-maxno;
     }
 };
