a5f6b7667ac1a432ea65ca658dafc3fd0922e0c7
Raushan Raj <2023ugee052@nitjsr.ac.in>
2025-08-26 02:26:34 +0530

    Time: 31 ms (54.93%), Space: 44.2 MB (31.08%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,4 +1,5 @@
-class Solution {
+//memoization
+/*class Solution {
 public:
 int dissub(int index1,int index2,vector<vector<int>> &dp,string &s, string &t){
     if(index2<0){return 1;}
@@ -16,5 +17,27 @@ int dissub(int index1,int index2,vector<vector<int>> &dp,string &s, string &t){
         int m=t.size();
         vector<vector<int>> dp(n,vector<int>(m,-1));
         return dissub(n-1,m-1,dp,s,t);
+    }
+};*/
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+        vector<vector<unsigned long long>> dp(n+1,vector<unsigned long long>(m+1,0));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return (int)dp[n][m];
     }
 };
