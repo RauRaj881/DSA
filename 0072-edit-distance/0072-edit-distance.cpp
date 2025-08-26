a82d3819010dae738ae4b64d4f0e8e781808563e
Raushan Raj <2023ugee052@nitjsr.ac.in>
2025-08-26 12:40:54 +0530

    Time: 4 ms (80.67%), Space: 12.9 MB (75.11%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,4 +1,5 @@
-class Solution {
+//memoization
+/*class Solution {
 public:
 int mind(int index1,int index2,vector<vector<int>> &dp,string &word1, string &word2){
     if(index1<0){return index2+1;}
@@ -19,5 +20,33 @@ int mind(int index1,int index2,vector<vector<int>> &dp,string &word1, string &wo
         int m=word2.size();
         vector<vector<int>> dp(n,vector<int>(m,-1));
         return mind(n-1,m-1,dp,word1,word2); 
+    }
+};*/
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n=word1.size();
+        int m=word2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=i;
+        }
+        for(int j=0;j<=m;j++){
+            dp[0][j]=j;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(word1[i-1]==word2[j-1]){
+        dp[i][j]=dp[i-1][j-1];
+            }
+       else{
+        int del=1+dp[i-1][j];
+        int insert=1+dp[i][j-1];
+        int replace=1+dp[i-1][j-1];
+        dp[i][j]=min(del,min(insert,replace));
+              }
+            }
+        }
+        return dp[n][m];
     }
 };
