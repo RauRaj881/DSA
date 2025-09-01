9701a7cbbf3a8fac31146aa800013e31548a858d
Raushan Raj <2023ugee052@nitjsr.ac.in>
2025-09-01 17:40:36 +0530

    Time: 284 ms (35.42%), Space: 296.5 MB (23.78%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,4 +1,5 @@
-class Solution {
+//memoization
+/*class Solution {
 public:
 int LIS(int index,int prev,vector<int>& nums,vector<vector<int>> &dp,int n){
     if(index==n){return 0;}
@@ -14,5 +15,24 @@ int LIS(int index,int prev,vector<int>& nums,vector<vector<int>> &dp,int n){
         int n=nums.size();
         vector<vector<int>> dp(n,vector<int>(n+1,-1));
         return LIS(0,-1,nums,dp,n);
+    }
+};*/
+//tabulation
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        for(int i=n-1;i>=0;i--){
+            for(int prev=-1;prev<i;prev++){
+                int nottake=dp[i+1][prev+1];
+             int take=0;
+            if(prev==-1||nums[i]>nums[prev]){
+            take=1+dp[i+1][i+1];
+            }
+          dp[i][prev+1]=max(take,nottake);
+            }
+        }
+        return dp[0][0];
     }
 };
