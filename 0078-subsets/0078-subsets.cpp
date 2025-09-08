dc75026ca6d7069722a08bf2c6b26a570b5ff41d
Raushan Raj <2023ugee052@nitjsr.ac.in>
2025-09-08 18:52:42 +0530

    Time: 1 ms (46.55%), Space: 9.8 MB (94.88%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,4 +1,5 @@
-class Solution {
+//using recursion
+/*class Solution {
 public:
     void backtrack(vector<vector<int>> &v,  vector<int> &ans, vector<int> nums,int i){
         if(i==nums.size()){v.push_back(ans);return;}
@@ -11,6 +12,25 @@ public:
         vector<vector<int>> v;
         vector<int> ans;
         backtrack(v,ans,nums,0);
+        return v;
+    }
+};*/
+//using power set technique
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n=nums.size();
+        int totss=1<<n;
+        vector<vector<int>> v;
+        for(int num=0;num<totss;num++){
+            vector<int> ans;
+            for(int i=0;i<n;i++){
+                if((num&(1<<i))!=0){
+                    ans.push_back(nums[i]);
+                }
+            }
+            v.push_back(ans);
+        }
         return v;
     }
 };
