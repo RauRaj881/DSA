5872c3d609de1f5b8cdbdc3f61af117c298d374b
Raushan Raj <2023ugee052@nitjsr.ac.in>
2025-10-01 19:19:38 +0530

    Time: 42 ms (41.07%), Space: 115.2 MB (8.12%) - LeetHub

---
+++ 1966-frequency-of-the-most-frequent-element/1966-frequency-of-the-most-frequent-element.cpp
@@ -0,0 +1,44 @@
+/*class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        vector<int> v;
+        for(int i=1;i<n;i++){
+            int p=nums[i]-nums[i-1];
+            v.push_back(p);
+        }
+        sort(v.begin(),v.end());
+        int sum=0,t=1;
+        for(int j=0;j<n-1;j++){
+            if(sum+v[j]<=k){sum+=v[j];t++;}
+            else break;
+        }
+        return t;
+    }
+};*/
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, long long k) {
+        long long n=nums.size();
+        sort(nums.begin(),nums.end());
+        long long l=0,r=0;
+        vector<long long> sumtill(n);
+        long long sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            sumtill[i]=sum;
+        }
+        long long ans=1;
+        while(r<n){
+            long long winsize=r-l+1;
+            long long st=(winsize*nums[r]-(sumtill[r]-sumtill[l]+nums[l]));
+            if(st<=k){
+                ans=max(ans,r-l+1);
+                r++;
+            }
+            else{l++;}
+        }
+        return ans;
+    }
+};
