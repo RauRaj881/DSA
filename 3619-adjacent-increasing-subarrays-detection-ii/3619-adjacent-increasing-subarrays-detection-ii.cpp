87d1f353a22e63dc3d4936a03ac1684f569d4e26
Raushan Raj <2023ugee052@nitjsr.ac.in>
2025-10-15 20:05:34 +0530

    Time: 228 ms (53.38%), Space: 173.7 MB (62.03%) - LeetHub

--- 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.cpp
+++ 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.cpp
@@ -1,3 +1,5 @@
+//O(n) space
+/*
 class Solution {
 public:
     int maxIncreasingSubarrays(vector<int>& nums) {
@@ -16,6 +18,24 @@ public:
                     ans=max(ans,cnt);
                 }
                 presum[i]=cnt;
+        }
+        return ans;
+    }
+};*/
+class Solution {
+public:
+    int maxIncreasingSubarrays(vector<int>& nums) {
+        int n=nums.size();
+        int cnt=1;
+        int precnt=1;
+        int ans=1;
+        for(int i=1;i<n;i++){
+            if(nums[i]>nums[i-1]){
+                cnt++;
+                ans=max(ans,cnt/2);
+                }
+                else{precnt=cnt;cnt=1;}
+                ans=max(ans,min(cnt,precnt));
         }
         return ans;
     }
