553a72240a33a9d8b2e5237cedb0863aae9566cd
Raushan Raj <2023ugee052@nitjsr.ac.in>
2025-10-29 17:58:25 +0530

    Time: 3 ms (7.67%), Space: 10.9 MB (40.14%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     int rob(vector<int>& nums) {
         int prev2=0;
@@ -28,5 +28,29 @@ public:
             pre=curr2;
         }
         return max(curr,curr2);
+    }
+};*/
+class Solution {
+public:
+int solve(vector<int> &nums){
+    int n=nums.size();
+        vector<int> dp(n,0);
+        for(int i=0;i<n;i++){
+            int take=nums[i];
+            if(i>1){take+=dp[i-2];}
+            int nottake=0;
+            if(i>0){
+            nottake+=dp[i-1];
+            }
+            dp[i]=max(take,nottake);
+        }
+        return dp[n-1];
+}
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1){return nums[0];}
+        vector<int> case1(nums.begin(),nums.end()-1);
+        vector<int> case2(nums.begin()+1,nums.end());
+        return max(solve(case1),solve(case2));
     }
 };
