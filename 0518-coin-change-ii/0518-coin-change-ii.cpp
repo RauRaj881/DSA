81c1838a291186e679966077c6c772fdae185425
Raushan Raj <2023ugee052@nitjsr.ac.in>
2025-11-05 20:41:54 +0530

    Time: 39 ms (27.57%), Space: 61.4 MB (10.93%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,5 +1,5 @@
 //memoization
-class Solution {
+/*class Solution {
 public:
 int cochange(int index,vector<vector<int>> &dp, vector<int>& coins,int amount){
     if(index==0){
@@ -21,15 +21,53 @@ int cochange(int index,vector<vector<int>> &dp, vector<int>& coins,int amount){
         vector<vector<int>> dp(n,vector<int>(5001,-1));
         return cochange(n-1,dp,coins,amount);
     }
-};/*
+};*/
+//space optimization
+/*class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        long long n=coins.size();
+        vector<long long> prev(amount+1,0);
+        for(long long sum=0;sum<=amount;sum++){
+            if(sum%coins[0]==0){prev[sum]=1;}
+        }
+        for(int i=1;i<n;i++){
+            vector<long long> curr(amount+1);
+            for(long long sum=0;sum<=amount;sum++){
+                long long nottake=prev[sum];
+                long long take=0;
+                if(coins[i]<=sum){
+                    take=curr[sum-coins[i]];
+                }
+                curr[sum]=take+nottake;
+            }
+            prev=curr;
+        }
+        return (int)prev[amount];
+    }
+};*/
 //tabulation
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(5001,-1));
-        return cochange(n-1,dp,coins,amount);
-
-        
+        vector<vector<unsigned long long>> dp(n, vector<unsigned long long>(amount+1, 0));
+        for(int sum=0;sum<=amount;sum++){
+            if(sum%coins[0]==0){
+                dp[0][sum]=1;
+            }
+        }
+        for(int i=1;i<n;i++){
+            for(int sum=0;sum<=amount;sum++){
+                long long nottake=dp[i-1][sum];
+                long long take=0;
+                if(coins[i]<=sum){
+                    take=dp[i][sum-coins[i]];
+                }
+                __int128 tmp = (__int128)take + (__int128)nottake;
+                dp[i][sum] = (long long)tmp;
+            }
+        }
+        return dp[n-1][amount];
     }
-};*/
+};
