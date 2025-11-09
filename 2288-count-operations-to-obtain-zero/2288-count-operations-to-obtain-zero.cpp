5af3e47a57fc3b742809cc10570b5ff4d083a618
Raushan Raj <2023ugee052@nitjsr.ac.in>
2025-11-09 16:00:18 +0530

    Time: 0 ms (100%), Space: 7.9 MB (71.74%) - LeetHub

--- 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.cpp
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     int countOperations(int num1, int num2) {
         int ans=0;
@@ -6,6 +6,23 @@ public:
             if(num1>=num2){num1-=num2;}
             else{num2-=num1;}
             ans++;
+        }
+        return ans;
+    }
+};*/
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        int ans=0;
+        while(num1!=0&&num2!=0){
+            if(num1>=num2){
+                ans+=num1/num2;
+                num1=num1%num2;
+            }
+            else{
+                ans+=num2/num1;
+                num2=num2%num1;
+            }
         }
         return ans;
     }
