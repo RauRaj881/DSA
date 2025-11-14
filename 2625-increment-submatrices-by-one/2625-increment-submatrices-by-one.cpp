bd267834081bc86bce9cbde17e2fea16fd553cfc
Raushan Raj <2023ugee052@nitjsr.ac.in>
2025-11-14 15:12:22 +0530

    Time: 26 ms (67.71%), Space: 91.4 MB (37.62%) - LeetHub

--- 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.cpp
+++ 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.cpp
@@ -1,3 +1,4 @@
+/* // O(n^2*m)
 class Solution {
 public:
     vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
@@ -9,6 +10,34 @@ public:
                 for(int c=min(curr[1],curr[3]);c<=max(curr[1],curr[3]);c++){
                     ans[r][c]++;
                 }
+            }
+        }
+        return ans;
+    }
+};*/
+// using difference array technique O(n^2+m);
+class Solution {
+public:
+    vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
+        int m=queries.size();
+        vector<vector<int>> ans(n,vector<int>(n,0));
+        for(int i=0;i<m;i++){
+            vector<int> curr=queries[i];
+            int r1=curr[0],c1=curr[1];
+            int r2=curr[2],c2=curr[3];
+            ans[r1][c1]++;
+            if(r2+1<n){ans[r2+1][c1]--;}
+            if(c2+1<n){ans[r1][c2+1]--;}
+            if(r2+1<n&&c2+1<n){ans[r2+1][c2+1]++;}
+        }
+        for(int r=0;r<n;r++){
+            for(int c=1;c<n;c++){
+                ans[r][c]+=ans[r][c-1];
+            }
+        }
+        for(int c=0;c<n;c++){
+            for(int r=1;r<n;r++){
+                ans[r][c]+=ans[r-1][c];
             }
         }
         return ans;
