5ea45cac4e3803125646aea7a6e761a4ca13d0a5
Raushan Raj <2023ugee052@nitjsr.ac.in>
2025-12-05 22:23:18 +0530

    Time: 113 ms (30.77%), Space: 194 MB (52.81%) - LeetHub

---
+++ 3623-count-number-of-trapezoids-i/3623-count-number-of-trapezoids-i.cpp
@@ -0,0 +1,47 @@
+/*class Solution {
+public:
+    int countTrapezoids(vector<vector<int>>& points) {
+        map<int,int> mp;
+        int n=points.size();
+        for(int i=0;i<n;i++){
+            mp[points[i][1]]++;
+        }
+        for(auto &it:mp){
+            if(it.second<2){mp.erase(it);}
+        }
+        int ans=1;
+        if(mp.size()<2){return 0;}
+        for(auto it:mp){
+            int x=it.second;
+            ans*=(x*(x-1))/2;
+        }
+        return ans;
+    }
+};*/
+class Solution {
+public:
+    int countTrapezoids(vector<vector<int>>& points) {
+        map<int,int> mp;
+        for (auto &p : points) {
+            mp[p[1]]++;
+        }
+        for (auto it = mp.begin(); it != mp.end();) {
+            if (it->second < 2) {
+                it = mp.erase(it);
+            } else {
+                ++it;
+            }
+        }
+        long long mod=1e9+7;
+        if (mp.size() < 2) return 0;
+        long long ans = 0;
+        long long sum = 0;
+        for (auto it : mp) {
+            long long x = it.second;
+            long long edge = (x * (x - 1) / 2) % mod;
+            ans = (ans + edge * sum) % mod;
+            sum = (sum + edge) % mod;
+        }
+        return ans % mod;
+    }
+};
