050138cd3037b190db576065001293c9f2b2134b
Raushan Raj <2023ugee052@nitjsr.ac.in>
2025-12-05 20:40:33 +0530

    Time: 0 ms (100%), Space: 22.5 MB (25%) - LeetHub

--- 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.cpp
+++ 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.cpp
@@ -5,10 +5,11 @@ public:
         int tot=0;
         for(auto it:nums){tot+=it;}
         int sum=0,ans=0;
-        for(int i=0;i<n-1;i++){
+        /*for(int i=0;i<n-1;i++){
             sum+=nums[i];
             if((sum-(tot-sum))%2==0){ans++;}
-        }
+        }*/
+        ans=(tot%2==0?n-1:0);
         return ans;
     }
 };
