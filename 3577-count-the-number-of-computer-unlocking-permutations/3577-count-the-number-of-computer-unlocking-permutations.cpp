ecb65aa0e183d63721119310318c8f1fb2372819
Raushan Raj <2023ugee052@nitjsr.ac.in>
2025-12-10 13:55:59 +0530

    Time: 3 ms (43.5%), Space: 92.7 MB (71%) - LeetHub

--- 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.cpp
+++ 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.cpp
@@ -2,20 +2,13 @@ class Solution {
 public:
     int countPermutations(vector<int>& complexity) {
         int n=complexity.size();
-        multiset<int> ms;
         long long ans=1;
         long long mod=1e9+7;
-        ms.insert(complexity[0]);
-        for(int i=1;i<n;i++){
-            int num=complexity[i];
-            auto it = ms.lower_bound(num);
-            if(it==ms.begin()){return 0;}
-        }
-        long long k=1;
-        for(int i=1;i<n;i++){
-            long long x=ans*k;
+        int mn=*min_element(complexity.begin()+1,complexity.end());
+        if(mn<=complexity[0]){return 0;}
+        for(long long i=1;i<n;i++){
+            long long x=ans*i;
             ans=(x)%(mod);
-            k++;
         }
         return (int)ans;
     }
