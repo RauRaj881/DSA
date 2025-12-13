95255c69b2a3764a2bbbd64547ddadd2e425df73
Raushan Raj <2023ugee052@nitjsr.ac.in>
2025-12-13 21:41:38 +0530

    Time: 0 ms (100%), Space: 8.5 MB (90.12%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -4,8 +4,7 @@ public:
         int n1=s.size();
         int n2=t.size();
         int i1=0,i2=0;
-        if(s==t){return true;}
-        else if(s.size()==0){return true;}
+        if(s.size()==0){return true;}
         while(i1<n1&&i2<n2){
             if(s[i1]==t[i2]){
                 i1++;i2++;
