1a436cc4e4f0e4e7cbf996d4fd67546b1b7b8a66
Raushan Raj <2023ugee052@nitjsr.ac.in>
2025-12-13 22:26:06 +0530

    Time: 88 ms (58.95%), Space: 57.6 MB (56.25%) - LeetHub

---
+++ 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.cpp
@@ -0,0 +1,48 @@
+//TLE
+/*class Solution {
+public:
+    int numMatchingSubseq(string s, vector<string>& words) {
+        int n1=s.size();
+        int cnt=0;
+        for(int i=0;i<words.size();i++){
+            int i1=0,i2=0;
+            string t=words[i];
+            int n2=t.size();
+        while(i1<n1&&i2<n2){
+            if(s[i1]==t[i2]){
+                i1++;i2++;
+            }
+            else{
+                i1++;
+            }
+            if(i2==n2){cnt++;}
+        }
+        }
+        return cnt;        
+    }
+};*/
+class Solution {
+public:
+    int numMatchingSubseq(string s, vector<string>& words) {
+        int n1=s.size();
+        unordered_map<char,vector<int>> mp;
+        for(int i=0;i<n1;i++){
+            mp[s[i]].push_back(i);
+        }
+        int cnt=0;
+        for(int i=0;i<words.size();i++){
+            string t=words[i];
+            int n2=t.size();
+            int i1=0,i2=0,prev=-1;
+            for(int j=0;j<n2;j++){
+                char c=t[j];
+                vector<int>& v=mp[c];
+                auto it=upper_bound(v.begin(),v.end(),prev);
+                if(it==v.end()){cnt--;break;}
+                else{prev=*it;}
+            }
+            cnt++;
+        }
+        return cnt;
+    }
+};
