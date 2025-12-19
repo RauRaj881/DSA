fc1dbcfa0cc2b2a9f0c87326e02880aa6674f81a
Raushan Raj <2023ugee052@nitjsr.ac.in>
2025-12-20 00:15:33 +0530

    Time: 0 ms (100%), Space: 9.1 MB (71.59%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.cpp
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
         int n=nums.size();
@@ -28,5 +28,38 @@ public:
             string z=x+"-"+">"+y;
             ans.push_back(z);}
             return ans;
+    }
+};*/
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        int n=nums.size();
+        if(n==0){return {};}
+        int st=nums[0];
+        vector<string> ans;
+        for(int i=1;i<n;i++){
+            if(nums[i]-1!=nums[i-1]){
+                if(st==nums[i-1]){
+                    ans.push_back(to_string(st));
+                }
+                else{
+                    string a=to_string(st);
+                    string b=to_string(nums[i-1]);
+                    string f=a+"-"+">"+b;
+                    ans.push_back(f);
+                }
+                st=nums[i];
+            }
+        }
+                if(st==nums[n-1]){
+                    ans.push_back(to_string(st));
+                }
+                else{
+                    string a=to_string(st);
+                    string b=to_string(nums[n-1]);
+                    string f=a+"-"+">"+b;
+                    ans.push_back(f);
+                }
+        return ans;
     }
 };
