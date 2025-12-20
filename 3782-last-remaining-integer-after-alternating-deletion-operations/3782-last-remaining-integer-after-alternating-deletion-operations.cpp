579eeed2c22a8a67f8cb3ad36405559251cf4b41
Raushan Raj <2023ugee052@nitjsr.ac.in>
2025-12-20 23:30:37 +0530

    Time: 0 ms (100%), Space: 9 MB (42.87%) - LeetHub

--- 3782-last-remaining-integer-after-alternating-deletion-operations/3782-last-remaining-integer-after-alternating-deletion-operations.cpp
+++ 3782-last-remaining-integer-after-alternating-deletion-operations/3782-last-remaining-integer-after-alternating-deletion-operations.cpp
@@ -3,12 +3,12 @@ public:
     long long lastInteger(long long n) {
         long long len=n,a=1,d=1,step=0;;
         while(len!=1){
-            d*=2;
-            step++;
-            len=(n-a)/d+1;
             if(step%2==1&&len%2==0){
                 a=a+d;
             }
+            d*=2;
+            step++;
+            len=(n-a)/d+1;
         }
         return a;   
     }
