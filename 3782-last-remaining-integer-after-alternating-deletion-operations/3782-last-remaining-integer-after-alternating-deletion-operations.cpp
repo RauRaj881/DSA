08ff0c64f6257d986f13ebb5766393300bb6be44
Raushan Raj <2023ugee052@nitjsr.ac.in>
2025-12-20 23:16:28 +0530

    Time: 0 ms (100%), Space: 9.1 MB (31.58%) - LeetHub

---
+++ 3782-last-remaining-integer-after-alternating-deletion-operations/3782-last-remaining-integer-after-alternating-deletion-operations.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long lastInteger(long long n) {
+        long long len=n,a=1,d=1,step=0;;
+        while(len!=1){
+            d*=2;
+            step++;
+            len=(n-a)/d+1;
+            if(step%2==1&&len%2==0){
+                a=a+d;
+            }
+        }
+        return a;   
+    }
+};
