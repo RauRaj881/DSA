51fb9bf6bd70d6d32f2cac9408bb8b263feca70d
Raushan Raj <2023ugee052@nitjsr.ac.in>
2025-12-22 02:01:35 +0530

    Time: 17 ms (5.29%), Space: 25.8 MB (5.29%) - LeetHub

--- 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.cpp
+++ 0955-delete-columns-to-make-sorted-ii/0955-delete-columns-to-make-sorted-ii.cpp
@@ -14,9 +14,12 @@ bool IsSorted(vector<string> &curr){
         int cnt=0;
         for(int i=0;i<m;i++){
             vector<string> curr=SortedSoFar;
+            int d=0;
             for(int j=0;j<n;j++){
                 curr[j]+=strs[j][i];
+                if(j<n-1&&strs[j][i]<strs[j+1][i]){d++;}
             }
+            if(d==n-1){return cnt;}
             if(IsSorted(curr)){
                 SortedSoFar=curr;
             }
