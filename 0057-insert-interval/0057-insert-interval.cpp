67a2e2e5931a7f34f3a6cd0183c3e48b1d2af511
Raushan Raj <2023ugee052@nitjsr.ac.in>
2025-12-22 20:01:44 +0530

    Time: 0 ms (100%), Space: 21.6 MB (93.74%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,45 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intv, vector<int>& newintv) {
-        int n=intv.size();
-        if(n==0){return {newintv};}
-        int l=newintv[0];
-        int h=newintv[1];
-        vector<vector<int>> ans;
-        int idx=n;
-        int low,high;
-        for(int i=0;i<n;i++){
-            int mn=intv[i][0];
-            int mx=intv[i][1];
-            if(!(mn>h||mx<l)){
-                low=min(mn,l);
-                high=max(mx,h);
-                idx=i;break;
-            }
+    vector<vector<int>> insert(vector<vector<int>>& inte, vector<int>& newinte) {
+        int n=inte.size();
+        vector<vector<int>> v;
+        int i=0;
+        while(i<n && inte[i][1]<newinte[0]){
+            v.push_back({inte[i][0],inte[i][1]});
+            i++;
         }
-        if(idx!=n){
-        for(int i=0;i<idx;i++){
-            ans.push_back(intv[i]);
+         while(i<n && inte[i][0]<=newinte[1]){
+           newinte[0]=min(inte[i][0],newinte[0]);
+           newinte[1]=max(inte[i][1],newinte[1]);
+            i++;
         }
+        v.push_back({newinte[0],newinte[1]});
+        while(i<n){
+            v.push_back({inte[i][0],inte[i][1]});
+            i++;
         }
-        for(int i=idx+1;i<n;i++){
-            int mn=intv[i][0];
-            int mx=intv[i][1];
-            if(!(mn>high||mx<low)){
-                low=min(mn,low);
-                high=max(mx,high);
-            }
-            else{
-                ans.push_back({low,high});
-                low=mn;high=mx;
-            }
-        }
-        if(idx!=n){ans.push_back({low,high});}
-        if(idx==n){
-            ans=intv;
-            ans.push_back(newintv);
-            sort(ans.begin(),ans.end());
-        }
-        return ans;
+        return v;
     }
 };
