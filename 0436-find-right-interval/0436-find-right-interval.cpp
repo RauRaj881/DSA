8a8a68935c99781f49690899677a453d518e2826
Raushan Raj <2023ugee052@nitjsr.ac.in>
2026-01-24 17:10:43 +0530

    Time: 6 ms (77.29%), Space: 28.7 MB (89.3%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.cpp
+++ 0436-find-right-interval/0436-find-right-interval.cpp
@@ -10,7 +10,7 @@ public:
         sort(v.begin(),v.end());
         vector<int> ans(n);
         for(int i=0;i<n;i++){
-            auto it=lower_bound(v.begin(),v.end(),make_pair(iv[i][1],-1e8));
+            auto it=lower_bound(v.begin(),v.end(),make_pair(iv[i][1],-1e6));
             if(it==v.end()){ans[i]=-1;}
             else{int idx=it-v.begin();ans[i]=v[idx].second;}
         }
