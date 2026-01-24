535e446e60e0da59e0ce76a03800b4b4c0999d6b
Raushan Raj <2023ugee052@nitjsr.ac.in>
2026-01-24 17:08:13 +0530

    Time: 11 ms (64.63%), Space: 28.6 MB (89.3%) - LeetHub

---
+++ 0436-find-right-interval/0436-find-right-interval.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> findRightInterval(vector<vector<int>>& iv) {
+        int n=iv.size();
+        vector<pair<int,int>> v(n);
+        for(int i=0;i<n;i++){
+            v[i].first=iv[i][0];
+            v[i].second=i;
+        }
+        sort(v.begin(),v.end());
+        vector<int> ans(n);
+        for(int i=0;i<n;i++){
+            auto it=lower_bound(v.begin(),v.end(),make_pair(iv[i][1],-1e8));
+            if(it==v.end()){ans[i]=-1;}
+            else{int idx=it-v.begin();ans[i]=v[idx].second;}
+        }
+        return ans;
+    }
+};
