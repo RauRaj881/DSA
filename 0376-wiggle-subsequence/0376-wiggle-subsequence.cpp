777d7047e881e26750fd1c2a4c883cdf44121312
Raushan Raj <2023ugee052@nitjsr.ac.in>
2026-01-24 16:41:27 +0530

    Time: 4 ms (19.4%), Space: 11 MB (18.4%) - LeetHub

---
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>> dp(n,vector<int>(2,0));
+        dp[0][0]=1;
+        dp[0][1]=1;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    dp[i][1]=max(dp[i][1],dp[j][0]+1);
+                }
+                else if(nums[i]<nums[j]){
+                    dp[i][0]=max(dp[i][0],dp[j][1]+1);
+                }
+            }
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans=max(ans,max(dp[i][0],dp[i][1]));
+        }
+        return ans;
+    }
+};
