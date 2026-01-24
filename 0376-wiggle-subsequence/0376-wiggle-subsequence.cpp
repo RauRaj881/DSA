f3020a567f2be53e3a65eef7bb20160be3cc0030
Raushan Raj <2023ugee052@nitjsr.ac.in>
2026-01-24 16:47:34 +0530

    Time: 0 ms (100%), Space: 10.1 MB (40.03%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
         int n=nums.size();
@@ -20,5 +20,17 @@ public:
             ans=max(ans,max(dp[i][0],dp[i][1]));
         }
         return ans;
+    }
+};*/
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        int n=nums.size();
+        int up=1,down=1;
+        for(int i=1;i<n;i++){
+            if(nums[i]>nums[i-1]){up=down+1;}
+            else if(nums[i]<nums[i-1]){down=up+1;}
+        }
+        return max(up,down);
     }
 };
