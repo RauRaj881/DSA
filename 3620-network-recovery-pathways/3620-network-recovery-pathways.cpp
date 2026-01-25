c27254b6e423dcf6711b98c1f5c0da123d5a845e
Raushan Raj <2023ugee052@nitjsr.ac.in>
2026-01-26 05:21:32 +0530

    Time: 535 ms (46.95%), Space: 443.5 MB (24.35%) - LeetHub

--- 3620-network-recovery-pathways/3620-network-recovery-pathways.cpp
+++ 3620-network-recovery-pathways/3620-network-recovery-pathways.cpp
@@ -1,8 +1,9 @@
-/*class Solution {
+class Solution {
 public:
-bool ispossible(long long mid,vector<vector<pair<long long,long long>>> &adj, vector<bool>& online,long long k,int n){
+bool ispossible(long long mid,vector<vector<pair<long long,long long>>> &adj,long long k,int n){
     set<pair<long long,long long>> s;
     s.insert({0,0});
+    vector<long long> mnd(n,k+1);
     while(!s.empty()){
         auto it=*s.begin();
         long long dist=it.first;
@@ -12,7 +13,11 @@ bool ispossible(long long mid,vector<vector<pair<long long,long long>>> &adj, ve
         for(auto ig:adj[nd]){
             long long v=ig.first;
             long long w=ig.second;
-            if(dist+w<=k&&w>=mid&&online[v]==true){s.insert({dist+w,v});}
+            if(dist+w<=k&&w>=mid&&dist+w<mnd[v]){
+                s.erase({mnd[v], v});
+                mnd[v]=dist+w;
+                s.insert({dist+w,v});
+            }
         }
     }
     return false;
@@ -24,87 +29,18 @@ bool ispossible(long long mid,vector<vector<pair<long long,long long>>> &adj, ve
             int u=edges[i][0];
             int v=edges[i][1];
             int w=edges[i][2];
-            adj[u].push_back({v,w});
+            if(online[v]&&online[u])adj[u].push_back({v,w});
         }
         long long low=0,high=1e9;
         long long ans=-1;
         while(low<=high){
             long long mid=(low+high)/2;
-            if(ispossible(mid,adj,online,k,n)){
+            if(ispossible(mid,adj,k,n)){
                 ans=mid;low=mid+1;
             }
             else{
                 high=mid-1;
             }
-        }
-        return ans;
-    }
-};*/
-#define ll long long
-class Solution {
-private:
-    bool isPathPossible(unordered_map<int, vector<pair<int, int>>>& graph, ll k,
-                        ll n, ll mid) {
-        // This function checks for existence of path bw 0 & n-1 & is within k
-        // cost
-        vector<ll> dist(n, k + 1);
-        priority_queue<pair<ll, int>, vector<pair<ll, int>>,
-                       greater<pair<ll, int>>>
-            pq;
-
-        dist[0] = 0;
-        pq.push({0, 0});
-
-        while (pq.size()) {
-            ll cost = pq.top().first;
-            int node = pq.top().second;
-            pq.pop();
-
-            if (cost > dist[node]) // Very Imp. For Pruning.
-                continue;
-
-            if (node == n - 1)
-                return true;
-
-            for (auto childData : graph[node]) {
-                int childNode = childData.first;
-                ll childCost = childData.second;
-
-                if (childCost < mid)
-                    continue;
-
-                if ((childCost + dist[node]) < dist[childNode]) {
-                    dist[childNode] = childCost + dist[node];
-                    pq.push({childCost, childNode});
-                }
-            }
-        }
-
-        return false;
-    }
-
-public:
-    int findMaxPathScore(vector<vector<int>>& edges, vector<bool>& online,
-                         long long k) {
-        ll start = 0, end = 1000000000, ans = -1, n = online.size();
-        // Construct Graph:
-        unordered_map<int, vector<pair<int, int>>> graph;
-        for (auto x : edges) {
-            int node1 = x[0], node2 = x[1], cost = x[2];
-            if (!online[node1] || !online[node2])
-                continue;
-            graph[node1].push_back({node2, cost});
-        }
-
-        while (start <= end) {
-            ll mid = (start + end) / 2;
-            // We will pick all the edges with wt >= mid as this has to be
-            // answer.
-            if (isPathPossible(graph, k, n, mid)) {
-                ans = max(ans, mid);
-                start = mid + 1;
-            } else
-                end = mid - 1;
         }
         return ans;
     }
