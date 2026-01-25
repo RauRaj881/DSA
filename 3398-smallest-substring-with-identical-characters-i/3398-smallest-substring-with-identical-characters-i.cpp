953eaef5e53f6f30472f428680f5b5dffa8c9aa5
Raushan Raj <2023ugee052@nitjsr.ac.in>
2026-01-25 13:41:56 +0530

    Time: 0 ms (100%), Space: 9.8 MB (20.55%) - LeetHub

---
+++ 3398-smallest-substring-with-identical-characters-i/3398-smallest-substring-with-identical-characters-i.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+bool ispossible(int mid,string s,int numOps){
+    if(mid==1){
+        int op1=0,op2=0;
+        for(int i=0;i<s.size();i++){
+            if(i%2==0){
+                if(s[i]=='0'){op1++;}
+                else{op2++;}
+            }
+            else{
+                if(s[i]=='1'){op1++;}
+                else{op2++;}
+            }
+        }
+        return min(op1,op2)<=numOps;
+    }
+    int cnt=1;
+    for(int i=1;i<s.size();i++){
+        if(s[i]==s[i-1]){cnt++;}
+        else{
+            numOps-=(cnt/(mid+1));
+            cnt=1;
+        }
+    }
+    numOps-=(cnt/(mid+1));
+    return numOps>=0;
+}
+    int minLength(string s, int numOps) {
+        int n=s.size();
+        int high=n;
+        int low=1;
+        int ans=n;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(ispossible(mid,s,numOps)){
+                ans=mid;
+                high=mid-1;
+            }
+            else{low=mid+1;}
+        }
+        return ans;
+    }
+};
