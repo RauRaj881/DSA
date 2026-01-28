656ef018fd7d350a6381b84d3f41d0dff5d1cd93
Raushan Raj <2023ugee052@nitjsr.ac.in>
2026-01-28 05:58:48 +0530

    Time: 52 ms (13.34%), Space: 115 MB (9.09%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -1,39 +1,19 @@
-/*class Solution {
-public:
-    int maxFrequency(vector<int>& nums, int k) {
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        vector<int> v;
-        for(int i=1;i<n;i++){
-            int p=nums[i]-nums[i-1];
-            v.push_back(p);
-        }
-        sort(v.begin(),v.end());
-        int sum=0,t=1;
-        for(int j=0;j<n-1;j++){
-            if(sum+v[j]<=k){sum+=v[j];t++;}
-            else break;
-        }
-        return t;
-    }
-};*/
+using ll = long long;
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, long long k) {
-        long long n=nums.size();
-        sort(nums.begin(),nums.end());
-        long long l=0,r=0;
+        int n=nums.size();
         vector<long long> sumtill(n);
-        long long sum=0;
-        for(int i=0;i<n;i++){
-            sum+=nums[i];
-            sumtill[i]=sum;
+        sort(nums.begin(),nums.end());
+        sumtill[0]=nums[0];
+        for(int i=1;i<n;i++){
+            sumtill[i]=nums[i]+sumtill[i-1];
         }
-        long long ans=1;
+        int l=0,r=0;
+        int ans=0;
         while(r<n){
-            long long winsize=r-l+1;
-            long long st=(winsize*nums[r]-(sumtill[r]-(l>0?sumtill[l-1]:0)));
-            if(st<=k){
+            long long diff=(ll)(r-l+1)*nums[r]-(sumtill[r]-(l>0?sumtill[l-1]:0));
+            if(diff<=k){
                 ans=max(ans,r-l+1);
                 r++;
             }
