99f115c319d069759cee99983737f77e6f460aaf
Raushan Raj <2023ugee052@nitjsr.ac.in>
2026-02-01 13:57:04 +0530

    Time: 12 ms (95.68%), Space: 15.3 MB (87.03%) - LeetHub

--- 1416-restore-the-array/1416-restore-the-array.cpp
+++ 1416-restore-the-array/1416-restore-the-array.cpp
@@ -2,18 +2,19 @@ class Solution {
 public:
     int numberOfArrays(string s, int k) {
         int n=s.size();
-        vector<int> dp(n+1);
-        dp[n] = 1; 
-        for (int i=n-1;i>=0;--i) {
-        if (s[i]=='0')
-            continue;
-           for (long sz=1,m=0;i+sz<=n;++sz) {
-               m=m*10+s[i+sz-1]-'0';
-               if (m > k)
-                   break;
-                dp[i] = (dp[i] + dp[i + sz]) % 1000000007;
+        vector<int> dp(n+1,0);
+        dp[n]=1;
+        int mod=1e9+7;
+        for(int i=n-1;i>=0;i--){
+            long long num=s[i]-'0';
+            if(num==0){continue;}
+            int j=i;
+            while(j<n&&num<=k){
+                dp[i]=(dp[i]+dp[j+1])%(mod);
+                j++;
+                num=num*10+(long long)(s[j]-'0');
             }
         }
-    return dp[0];
-}
+        return dp[0];
+    }
 };
