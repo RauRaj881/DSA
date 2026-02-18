3d3d6b5f803b73c98a7b4b91545f0cd2feb05c0f
Raushan Raj <2023ugee052@nitjsr.ac.in>
2026-02-18 13:08:49 +0530

    Time: 0 ms (100%), Space: 7.8 MB (62.63%) - LeetHub

---
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool hasAlternatingBits(int n) {
+        int x=1;
+        int cr;
+        if((n&1)==0){cr=0;}
+        else{cr=1;}
+        x<<=1;
+        int dr;
+        for(int i=1;i<31;i++){
+            if(cr==1){
+               if((x&n)!=0){return false;}
+               else{cr=0;x<<=1;} 
+            }
+            else{
+                if((x&n)!=0){x<<=1;cr=1;}
+                else{dr=i;break;}
+            }
+        }
+        for(int i=dr;i<31;i++){
+            if((x&n)!=0){return false;}
+            else{x<<=1;}
+        }
+        return true;
+    }
+};
