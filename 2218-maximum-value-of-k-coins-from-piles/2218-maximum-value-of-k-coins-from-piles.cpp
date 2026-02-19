5588bb8450ec4aa702fd855f0c4e79e2e79524b8
Raushan Raj <2023ugee052@nitjsr.ac.in>
2026-02-20 04:36:20 +0530

    Time: 175 ms (19.24%), Space: 65.9 MB (5.08%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
@@ -17,8 +17,8 @@ public:
         }
         for(int i=1;i<n;i++){
             for(int j=1;j<=k;j++){
-                for(int x=0;x<min(j,(int)piles[i].size());x++){
-                    dp[i][j]=max(dp[i][j],dp[i-1][j-x-1]+pre[i][x]);
+                for(int x=1;x<=min(j,(int)piles[i].size());x++){
+                    dp[i][j]=max(dp[i][j],dp[i-1][j-x]+pre[i][x-1]);
                 }
                 dp[i][j]=max(dp[i][j],dp[i-1][j]);
             }
