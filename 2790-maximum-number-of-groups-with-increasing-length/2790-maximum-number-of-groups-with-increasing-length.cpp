ea68121dee29ca10ac6d5b94dbff2c9a14bfdf4d
Raushan Raj <2023ugee052@nitjsr.ac.in>
2026-02-20 17:07:25 +0530

    Time: 55 ms (12.7%), Space: 129.7 MB (69.84%) - LeetHub

--- 2790-maximum-number-of-groups-with-increasing-length/2790-maximum-number-of-groups-with-increasing-length.cpp
+++ 2790-maximum-number-of-groups-with-increasing-length/2790-maximum-number-of-groups-with-increasing-length.cpp
@@ -3,17 +3,17 @@ public:
 bool ispossible(int mid,vector<int>& uL){
     int n=uL.size();
     if(n<mid){return false;}
-    int cr=mid;
+    int req=mid;
     int gap=0;
     for(int i=0;i<n;i++){
-        gap=max(0,cr-uL[i]+gap);
-        if(cr>0)cr--;
+        gap=max(0,req-uL[i]+gap);
+        if(req>0)req--;
     }
     return gap==0;
 }
     int maxIncreasingGroups(vector<int>& uL){
         int n=uL.size();
-        int low=0,high=1e9;
+        int low=0,high=n;
         int ans=0;
         sort(uL.begin(),uL.end(),greater<int>());
         while(low<=high){
