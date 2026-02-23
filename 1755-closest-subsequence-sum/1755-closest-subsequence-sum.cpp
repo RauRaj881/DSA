ec4f5304155b0b7fbbe74df98846c2a1e2f7b192
Raushan Raj <2023ugee052@nitjsr.ac.in>
2026-02-23 14:22:18 +0530

    Time: 1750 ms (5.02%), Space: 217.4 MB (5.36%) - LeetHub

--- 1755-closest-subsequence-sum/1755-closest-subsequence-sum.cpp
+++ 1755-closest-subsequence-sum/1755-closest-subsequence-sum.cpp
@@ -35,20 +35,11 @@ void storesm(int st,int r,map<int, int>&dp,int crsm,vector<int>&nums){
     storesm(st + 1, r, dp,crsm,nums);
     storesm(st + 1, r, dp,crsm + nums[st],nums);
 }
-void storesm2(int st,int r,unordered_map<int, int>&dp,int crsm,vector<int>&nums){
-    if(st>r){
-        dp[crsm]++;
-        return;
-    }
-    storesm2(st + 1, r, dp,crsm,nums);
-    storesm2(st + 1, r, dp,crsm + nums[st],nums);
-}
     int minAbsDifference(vector<int>& nums, int x) {
         int n=nums.size();
         map<int,int> dp1;
         storesm(0, n/2-1, dp1,0,nums);
         map<int,int> dp2;
-        //dp2.reserve(1 << 20);
         storesm(n / 2, n - 1, dp2,0,nums);
         int ans = 0;
         int mn=1e9;
@@ -64,7 +55,7 @@ void storesm2(int st,int r,unordered_map<int, int>&dp,int crsm,vector<int>&nums)
                 auto pit = prev(it);
                 int left = pit->first;
                 mn = min(mn, abs((left + right) - x));
-               }
+            }
         }
         return mn; 
     }
