35ae89d4e738c2fd96e7a54d8f4e08358cfb97b6
Raushan Raj <2023ugee052@nitjsr.ac.in>
2026-02-23 14:17:58 +0530

    Time: 1695 ms (5.02%), Space: 217.5 MB (5.36%) - LeetHub

--- 1755-closest-subsequence-sum/1755-closest-subsequence-sum.cpp
+++ 1755-closest-subsequence-sum/1755-closest-subsequence-sum.cpp
@@ -47,9 +47,9 @@ void storesm2(int st,int r,unordered_map<int, int>&dp,int crsm,vector<int>&nums)
         int n=nums.size();
         map<int,int> dp1;
         storesm(0, n/2-1, dp1,0,nums);
-        unordered_map<int,int> dp2;
-        dp2.reserve(1 << 20);
-        storesm2(n / 2, n - 1, dp2,0,nums);
+        map<int,int> dp2;
+        //dp2.reserve(1 << 20);
+        storesm(n / 2, n - 1, dp2,0,nums);
         int ans = 0;
         int mn=1e9;
         for(auto it2 : dp2){
