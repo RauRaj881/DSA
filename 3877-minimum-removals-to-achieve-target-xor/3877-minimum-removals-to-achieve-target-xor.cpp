cd8754fc3766b364f8d9f8c61414c79bc5a1bd6a
Raushan Raj <2023ugee052@nitjsr.ac.in>
2026-03-22 23:46:27 +0530

    Time: 2219 ms (5%), Space: 237.6 MB (43.44%) - LeetHub

--- 3877-minimum-removals-to-achieve-target-xor/3877-minimum-removals-to-achieve-target-xor.cpp
+++ 3877-minimum-removals-to-achieve-target-xor/3877-minimum-removals-to-achieve-target-xor.cpp
@@ -1,19 +1,19 @@
-class Solution {
-public:
-    int minRemovals(vector<int>& nums, int target) {
-        int n=nums.size();
-        vector<vector<int>> dp(n,vector<int>(2e4+1,-1));
-        dp[0][nums[0]]=1;
-        if(nums[0]!=0)dp[0][0]=0;
-        for(int i=1;i<n;i++){
-            for(int j=0;j<=1e4;j++){
-                if(dp[i-1][j]==-1){continue;}
-                int xr=j^nums[i];
-                dp[i][j]=max(dp[i][j],dp[i-1][j]);
-                dp[i][xr]=max(dp[i][xr],dp[i-1][j]+1);
-            }
+class Solution { 
+public: 
+    int minRemovals(vector<int>& nums, int target) { 
+        int n=nums.size(); 
+        long long sz=1<<14; 
+        vector<unordered_map<int,long long>> dp(n); 
+        dp[0][nums[0]]=1; 
+        if(nums[0]!=0)dp[0][0]=0; 
+        for(int i=1;i<n;i++){ 
+            for(int j=0;j<=1e4;j++){ 
+                int xr=j^nums[i]; 
+                if(dp[i-1].count(j)){dp[i][j]=dp[i-1][j];} 
+                if(dp[i-1].count(xr)){dp[i][j]=max(dp[i-1][xr]+1,dp[i][j]);}
+            } 
         }
-        if(dp[n-1][target]==-1){return -1;}
-        return n-dp[n-1][target];
-    }
+        if(!dp[n-1].count(target)){return -1;} 
+        return n-dp[n-1][target]; 
+    } 
 };
