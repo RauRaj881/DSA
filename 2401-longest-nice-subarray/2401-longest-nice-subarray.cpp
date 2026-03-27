8773f76b8402fe29b3d0a215dce400001da8d698
Raushan Raj <2023ugee052@nitjsr.ac.in>
2026-03-27 14:20:14 +0530

    Time: 0 ms (100%), Space: 61 MB (27.86%) - LeetHub

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-    int longestNiceSubarray(vector<int>& nums) {
+    int longestNiceSubarray(vector<int>& nums){
         int n=nums.size();
+        int l=0;
+        int nm=0;
         int ans=0;
-        for(int i=0;i<n;i++){
-            unordered_set<int> st;
-            for(int j=i;j<min(n,i+30);j++){
-                bool pos=true;
-                for(int k=0;k<=30;k++){
-                    if((nums[j] & (1<<k))!=0){
-                        if(st.count(k)){
-                        pos=false;break;}
-                        else{st.insert(k);}
-                    }
-                }
-                if(!pos){break;}
-                ans=max(ans,j-i+1);
+        for(int r=0;r<n;r++){
+            while((nm&nums[r])!=0){
+                nm^=nums[l];
+                l++;
             }
+            nm|=nums[r];
+            ans=max(ans,r-l+1);
         }
         return ans;
     }
