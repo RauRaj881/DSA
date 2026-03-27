ec3020dfd9e4555fdd8db1ba2ae0479c8240ceef
Raushan Raj <2023ugee052@nitjsr.ac.in>
2026-03-27 13:47:22 +0530

    Time: 1805 ms (5.07%), Space: 464.8 MB (6.59%) - LeetHub

---
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int longestNiceSubarray(vector<int>& nums) {
+        int n=nums.size();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            unordered_set<int> st;
+            for(int j=i;j<min(n,i+30);j++){
+                bool pos=true;
+                for(int k=0;k<=30;k++){
+                    if(nums[j] & (1<<k)){
+                        if(st.count(k)){
+                        pos=false;break;}
+                        else{st.insert(k);}
+                    }
+                }
+                if(!pos){break;}
+                ans=max(ans,j-i+1);
+            }
+        }
+        return ans;
+        
+    }
+};
