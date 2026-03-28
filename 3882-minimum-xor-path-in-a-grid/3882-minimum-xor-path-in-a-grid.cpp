fe69d93d7e678065dd237423dc7b04ea716be3b8
Raushan Raj <2023ugee052@nitjsr.ac.in>
2026-03-28 23:08:10 +0530

    Time: 674 ms (32.81%), Space: 62.1 MB (70.26%) - LeetHub

--- 3882-minimum-xor-path-in-a-grid/3882-minimum-xor-path-in-a-grid.cpp
+++ 3882-minimum-xor-path-in-a-grid/3882-minimum-xor-path-in-a-grid.cpp
@@ -8,12 +8,16 @@ public:
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 for(int k=0;k<1024;k++){
-                    if(!dp[i][j][k]){continue;}
-                    if(i+1<n){
-                        dp[i+1][j][k^grid[i+1][j]]=true;
+                    if(i==0&&j==0&&k==grid[0][0]){dp[i][j][k]=true;continue;}
+                    if(j>0){
+                        if(dp[i][j-1][k^grid[i][j]]){
+                            dp[i][j][k]=true;
+                        }
                     }
-                    if(j+1<m){
-                        dp[i][j+1][k^grid[i][j+1]]=true;
+                    if(i>0){
+                        if(dp[i-1][j][k^grid[i][j]]){
+                            dp[i][j][k]=true;
+                        }
                     }
                 }
             }
