18035e87a232d2c741d42abd3a3eb10a63024968
Raushan Raj <2023ugee052@nitjsr.ac.in>
2026-03-30 18:07:51 +0530

    Time: 72 ms (5.17%), Space: 61.4 MB (5.75%) - LeetHub

---
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool checkStrings(string s1, string s2) {
+        int n=s2.size();
+        if(n==1) return s1[0]==s2[0];
+        vector<string> v(n);
+        for(int i=0;i<=1;i++){
+            string s="";
+            for(int j=i;j<n;j+=2){
+                //string p=s1[j];
+                s+=s1[j];
+            }
+            sort(s.begin(),s.end());
+            v[i]=s;
+        }
+        for(int i=0;i<2;i++){
+            string s="";
+            for(int j=i;j<n;j+=2){
+                //string p=s1[j];
+                s+=s2[j];
+            }
+            sort(s.begin(),s.end());
+            if(s!=v[i]) return false;
+        }
+        return true;
+        
+    }
+};
