f6173768922c6f6fbb50f0734a252c9d49ea592b
Raushan Raj <2023ugee052@nitjsr.ac.in>
2026-03-30 19:42:19 +0530

    Time: 395 ms (5.03%), Space: 528 MB (5.74%) - LeetHub

--- 2573-find-the-string-with-lcp/2573-find-the-string-with-lcp.cpp
+++ 2573-find-the-string-with-lcp/2573-find-the-string-with-lcp.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     string findTheString(vector<vector<int>>& lcp){
         int n=lcp.size();
-        string s(n, '\0');
-        char current = 'a';
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
                 if(i==j){if(lcp[i][j]!=n-i){return "";}}
@@ -11,18 +9,15 @@ public:
                 else if(lcp[i][j]>min(n-i,n-j)){return "";}
             }
         }
-        for (int i = 0; i < n; i++) {
-            if (s[i] == '\0') {
-                if (current > 'z') {
-                    return "";
-                }
-                s[i] = current;
-                for (int j = i + 1; j < n; j++) {
-                    if (lcp[i][j] > 0) {
-                        s[j] = s[i];
-                    }
-                }
-                current++;
+        string s(n,'*');
+        char cr='a';
+        for(int i=0;i<n;i++){
+            if(s[i]=='*'){
+                if(cr>'z'){return "";}
+                s[i]=cr;cr++;
+            }
+            for(int j=i+1;j<n;j++){
+                if(lcp[i][j]>0){s[j]=s[i];}
             }
         }
         for(int i=0;i<n-1;i++){
