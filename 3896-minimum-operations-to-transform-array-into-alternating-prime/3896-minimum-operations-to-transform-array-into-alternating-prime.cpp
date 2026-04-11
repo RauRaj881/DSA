800e206fc2330d232fe5e00c77a771b3dd6d9e4e
Raushan Raj <2023ugee052@nitjsr.ac.in>
2026-04-11 23:12:43 +0530

    Time: 551 ms (22.23%), Space: 302.6 MB (11.25%) - LeetHub

--- 3896-minimum-operations-to-transform-array-into-alternating-prime/3896-minimum-operations-to-transform-array-into-alternating-prime.cpp
+++ 3896-minimum-operations-to-transform-array-into-alternating-prime/3896-minimum-operations-to-transform-array-into-alternating-prime.cpp
@@ -1,32 +1,26 @@
 typedef long long ll;
-static ll v[1000005];
-    static ll p[1000005];
-bool pos=false;
-    void f(){
-        if(pos){return;}
-        v[0]=v[1]=1;
-        for(ll i=2;i<1e6;i++){
-            //if(v[i]==0){
-            for(ll j=i*i;j<=1e6;j++){
-                if(j%i==0){v[j]=1;}
-            //}
-            }
-        }
-        int lt=-1;
-        for(ll i=1e6;i>=0;i--){
-            if(v[i]==0){lt=i;}
-            p[i]=lt;
-        }
-        pos=true;
-    }
 class Solution {
 public:
     int minOperations(vector<int>& nums){
         int n=nums.size();
-        f();
-        int ans=0;
-        for(int i=0;i<n;i++){
-            int x=nums[i];
+        vector<ll> v(1e5+5,0);
+        v[0]=v[1]=1;
+        for(ll i=2;i<1e5+5;i++){
+            if(v[i]==0){
+            for(ll j=i*i;j<=1e5+4;j+=i){
+                v[j]=1;
+            }
+            }
+        }
+        ll lt=-1;
+        vector<ll> p(1e5+5,-1);
+        for(ll i=1e5+4;i>=0;i--){
+            if(v[i]==0){lt=i;}
+            p[i]=lt;
+        }
+        ll ans=0;
+        for(ll i=0;i<n;i++){
+            ll x=nums[i];
             if(i%2==1){
                 if(v[x]==0){
                     if(x==2){ans+=2;}
@@ -35,10 +29,10 @@ public:
             }
             else{
                 if(v[x]==1){
-                    ans+=(p[x]-x);
+                    ans+=(ll)(p[x]-x);
                 }
             }
         }
-        return ans;
+        return (int)ans;
     }
 };
