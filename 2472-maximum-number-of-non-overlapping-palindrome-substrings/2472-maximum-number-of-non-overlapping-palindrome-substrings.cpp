4e35e0bed30cfccbb4341e415674d2435728f34c
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-04-14 21:52:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
+bool palin(int i,int j,string &s){
+    while(i<j){
+        if(s[i]!=s[j]){return false;}
+        i++;j--;
+    }
+    return true;
+}
     int maxPalindromes(string s, int k){
         int n=s.size();
-        vector<vector<bool>> palin(n, vector<bool>(n, false));
-        for(int i = n-1; i >= 0; i--) {
-            for(int j = i; j < n; j++) {
-                if(s[i] == s[j] && (j - i <= 2 || palin[i+1][j-1])) {
-                    palin[i][j] = true;
-                }
-            }
-        }
         vector<int> dp(n,0);
         for(int i=0;i<n;i++){
             for(int j=i+k-1;j<n;j++){
-                if(palin[i][j]){
+                if(palin(i,j,s)){
                     dp[j]=max(dp[j],(i>0?dp[i-1]:0)+1);
                 }
                 if(j>0)dp[j]=max(dp[j],dp[j-1]);
