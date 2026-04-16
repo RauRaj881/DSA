8e00b22f6de37a9bccfa747679a4668f3346c5fc
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-04-17 01:07:56 +0530

    Time: 43 ms (85.05%), Space: 29.1 MB (45.26%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,51 +1,21 @@
-//TLE O(N^2*LOG(N))
-/*class Solution {
-public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        set<vector<int>> st;
-        for(int i=0;i<n;i++){
-            int target=-nums[i];
-            unordered_map<int,int> mp;
-            mp[nums[i]]=i;
-            for(int j=i+1;j<n;j++){
-                if(mp.count(target-nums[j])&&mp[target-nums[j]]!=i){
-                    vector<int> temp={nums[i],nums[j],nums[mp[target-nums[j]]]};
-                    sort(temp.begin(),temp.end());
-                    st.insert(temp);
-                }
-                mp[nums[j]]=j;
-            }
-        }
-        vector<vector<int>> ans(st.begin(),st.end());
-        return ans;
-    }
-};*/
-//USING 2 POINTERS
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> threeSum(vector<int>& nums){
         int n=nums.size();
         sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
         for(int i=0;i<n;i++){
             if(i>0&&nums[i]==nums[i-1]){continue;}
+            int tar=-nums[i];
             int j=i+1,k=n-1;
-            int target=-nums[i];
             while(j<k){
-                if(nums[j]+nums[k]==target){
-                    ans.push_back({nums[i],nums[j],nums[k]});
-                    j++;k--;
-                    while(j<k&&nums[j]==nums[j-1]){j++;}
-                    while(j<k&&nums[k]==nums[k+1]){k--;}
+                if(nums[j]+nums[k]==tar){ans.push_back({nums[i],nums[j],nums[k]});
+                j++;k--;
+                while(j<k&&nums[j]==nums[j-1]){j++;}
+                while(j<k&&nums[k]==nums[k+1]){k--;}
                 }
-                else if(nums[j]+nums[k]<target){
-                    j++;
-                }
-                else{
-                    k--;
-                }
+                else if(nums[j]+nums[k]<tar){j++;}
+                else{k--;}
             }
         }
         return ans;
