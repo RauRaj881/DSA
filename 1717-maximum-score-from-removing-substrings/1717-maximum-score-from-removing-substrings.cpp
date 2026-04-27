3491e1d2c202c2d83f28bc46ac9ec9e75a06476c
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-04-27 15:21:54 +0530

    Time: 87 ms (14.11%), Space: 31.8 MB (27.93%) - LeetHub

--- 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.cpp
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
+int f(string &s,int vl,string tar){
+    stack<char> st;
+    int x=0;
+    for(int i=0;i<s.size();i++){
+        if(!st.empty()&&st.top()==tar[0]&&s[i]==tar[1]){
+            x+=vl;
+            st.pop();
+        }
+        else{st.push(s[i]);}
+    }
+    string tp="";
+    while(!st.empty()){
+        tp+=st.top();
+        st.pop();
+    }
+    reverse(tp.begin(),tp.end());
+    s=tp;
+    return x;
+}
     int maximumGain(string s, int x, int y){
-        int n=s.size();
-        int cnta=0,cntb=0;
         int ans=0;
         if(y>x){
             swap(x,y);
             reverse(s.begin(),s.end());
         }
-        for(int i=0;i<n;i++){
-            if(s[i]=='a'){cnta++;}
-            else if(s[i]=='b'){
-                if(cnta>0){cnta--;ans+=x;}
-                else{cntb++;}
-            }
-            else{ans+=min(cnta,cntb)*y;cnta=0;cntb=0;}
-        }
-        ans+=min(cnta,cntb)*y;
+        ans+=f(s,x,"ab");
+        ans+=f(s,y,"ba");
         return ans;
     }
 };
