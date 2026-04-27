be59267a084c59378dd167e3f253e50f4a021c84
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-04-27 16:44:14 +0530

    Time: 0 ms (100%), Space: 26.3 MB (33.94%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -7,7 +7,7 @@ public:
             int mid=(low+high)/2;
             if(mid%2==1){mid--;}
             if(nums[mid]==nums[mid+1]){low=mid+2;}
-            else{high=mid;}
+            else{high=mid-1;}
         }
         return nums[low];
         
