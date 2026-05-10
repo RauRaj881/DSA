d8928996ad59bd64d64163edfce007278d01a403
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-05-10 10:37:48 +0530

    Time: 106 ms (65.68%), Space: 234.9 MB (80.93%) - LeetHub

--- 3926-count-valid-word-occurrences/3926-count-valid-word-occurrences.cpp
+++ 3926-count-valid-word-occurrences/3926-count-valid-word-occurrences.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
     vector<int> countWordOccurrences(vector<string>& ch,vector<string>& qr){
-        int n=ch.size();
         int q=qr.size();
         string s;
         for(auto it:ch){
             s+=it;
         }
+        int n=s.size();
         unordered_map<string,int> mp;
         string tp="";
         for(int i=0;i<s.size();i++){
-            if (islower(s[i])) {
-                tp += s[i];
-            } 
-            else if (s[i] == '-' && i > 0 && i < s.size() - 1 && islower(s[i-1]) && islower(s[i+1])) {
-                tp += s[i];
-            } 
-            else {
-                mp[tp]++;
-                tp = "";
+            if(islower(s[i])){
+                tp+=s[i];
             }
+            else if(s[i]=='-'){
+                if(i>0&&i<n-1){
+                    if(islower(s[i-1])&&islower(s[i+1])){
+                        tp+=s[i];
+                    }
+                    else{mp[tp]++;tp="";}
+                }
+                else{mp[tp]++;tp="";}
+            }
+            else{mp[tp]++;tp="";}
         }
         mp[tp]++;
         vector<int> ans(q);
@@ -27,6 +30,5 @@ public:
             ans[i]=mp[qr[i]];
         }
         return ans;
-        
     }
 };
