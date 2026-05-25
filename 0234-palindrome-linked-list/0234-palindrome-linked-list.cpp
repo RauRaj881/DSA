49594570fcf3fbcfaa731137d09c3c9250992f6b
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-05-25 22:54:46 +0530

    Time: 0 ms (100%), Space: 118 MB (81.16%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,44 +10,29 @@
  */
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-       /* stack<int> st;
-        ListNode* temp=head;
-        while(temp!=nullptr){
-            st.push({temp->val});
-            temp=temp->next;
+    bool isPalindrome(ListNode* head){
+        ListNode* ft=head;
+        ListNode* sl=head;
+        while(ft!=nullptr&&ft->next!=nullptr){
+            ft=ft->next->next;
+            sl=sl->next;
         }
-        temp=head;
-        while(!st.empty()){
-            if(st.top()==temp->val){temp=temp->next;st.pop();}
-            else{return 0;}
+        ListNode* prv=nullptr;
+        ListNode* cr=sl;
+        while(cr!=nullptr){
+            ListNode* nt=cr->next;
+            cr->next=prv;
+            prv=cr;
+            cr=nt;
         }
-        return 1;*/
-        if(head==nullptr || head->next==nullptr){return 1;}
-        else if(head->next->next==nullptr && head->val!=head->next->val){return 0;}
-        ListNode* slow=head;
-        ListNode* fast=head;
-        ListNode* temp;
-        while(fast->next!=nullptr && fast->next->next!=nullptr){
-            fast=fast->next->next;
-            slow=slow->next;
-        }
-        ListNode* curr=slow->next;
-        ListNode* prev=nullptr;
-        while(curr!=nullptr){
-            temp=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=temp;
-        }
-        //slow->next=prev;
-        ListNode* second=prev;
+        ListNode* second=prv;
         ListNode* first=head;
         while(second!=nullptr){
-            if(first->val!=second->val){return 0;}
-                first=first->next;
-                second=second->next;
-            }
-        return 1;
+            if(second->val!=first->val){return false;}
+            second=second->next;
+            first=first->next;
+        }
+        return true;
+        
     }
 };
