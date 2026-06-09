a859a3ddf5c338d64cc1ffb4ec3671ce1c7f57fc
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-06-09 22:06:39 +0530

    Time: 0 ms (100%), Space: 8.3 MB (24.06%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -2,16 +2,7 @@ class Solution {
 public:
     bool rotateString(string s, string g){
         if(s.size()!=g.size()){return false;}
-        int n=s.size();
-        for(int st=0;st<n;st++){
-            int i=st,j=0;
-            int cnt=0;
-            while(cnt!=n){
-                if(s[i]!=g[j]){break;}
-                else{i=(i+1)%n;j++;cnt++;}
-            }
-            if(cnt==n){return true;}
-        }
-        return false;
+        string db=s+s;
+        return db.find(g)!=string::npos;
     }
 };
