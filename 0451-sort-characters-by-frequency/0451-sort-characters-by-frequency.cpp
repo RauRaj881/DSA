610fd3e831cdc6dafb8cab3d123cfb85d42bdfd7
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-06-12 08:22:47 +0530

    Time: 63 ms (5.06%), Space: 10.4 MB (95.7%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     string frequencySort(string s){
-        int n=s.size();
         unordered_map<char,int> mp;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<s.size();i++){
             mp[s[i]]++;
         }
-        sort(s.begin(),s.end(),[&](char x,char y){
-            if(mp[x]==mp[y]){return x<y;}
-            return mp[x]>mp[y];
+        sort(s.begin(),s.end(),[&](char a,char b){
+            if(mp[a]==mp[b]){return b>a;}
+            return mp[a]>mp[b];
         });
-        return s;
+        return s; 
     }
 };
