19871e2dd7114117120385edbe2f5fbb154335bb
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-06-14 16:24:37 +0530

    Time: 1630 ms (29.52%), Space: 472.9 MB (59.1%) - LeetHub

--- 3960-frequency-balance-subarray/3960-frequency-balance-subarray.cpp
+++ 3960-frequency-balance-subarray/3960-frequency-balance-subarray.cpp
@@ -5,34 +5,22 @@ public:
         int ans=1;
         for(int i=0;i<n;i++){
             unordered_map<int,int> mp;
-            unordered_map<int,int> st; // freq -> count
-
+            unordered_map<int,int> st;
             for(int j=i;j<n;j++){
-
-                int old = mp[nums[j]];
-                if(old){
-                    st[old]--;
-                    if(st[old]==0) st.erase(old);
+                if(mp[nums[j]]>0){
+                    st[mp[nums[j]]]--;
+                    if(st[mp[nums[j]]]==0){st.erase(mp[nums[j]]);}
                 }
-
                 mp[nums[j]]++;
                 st[mp[nums[j]]]++;
-
-                if(mp.size()==1){
-                    ans=max(ans,j-i+1);
-                }
-                else{
-                    if(st.size()==2){
-                        auto it=st.begin();
-                        int mn=it->first;
-                        ++it;
-                        int mx=it->first;
-
-                        if(mx<mn) swap(mx,mn);
-
-                        if(mx==2*mn)
-                            ans=max(ans,j-i+1);
+                if(mp.size()==1){ans=max(ans,j-i+1);}
+                else if(st.size()==2){
+                    int mx=0,mn=1e5;
+                    for(auto it:st){
+                        mx=max(mx,it.first);
+                        mn=min(mn,it.first);
                     }
+                    if(mx==2*mn){ans=max(ans,j-i+1);}
                 }
             }
         }
