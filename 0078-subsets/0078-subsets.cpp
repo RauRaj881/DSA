0e09404ddb1d4b2004dc713c237cf70886db5643
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-06-16 19:14:33 +0530

    Time: 0 ms (100%), Space: 10 MB (37.42%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,36 +1,16 @@
-//using recursion
-/*class Solution {
-public:
-    void backtrack(vector<vector<int>> &v,  vector<int> &ans, vector<int> nums,int i){
-        if(i==nums.size()){v.push_back(ans);return;}
-        ans.push_back(nums[i]);
-        backtrack(v,ans,nums,i+1);
-        ans.pop_back();
-        backtrack(v,ans,nums,i+1);
-    }
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> v;
-        vector<int> ans;
-        backtrack(v,ans,nums,0);
-        return v;
-    }
-};*/
-//using power set technique
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
+    vector<vector<int>> subsets(vector<int>& nums){
         int n=nums.size();
-        int totss=1<<n;
-        vector<vector<int>> v;
-        for(int num=0;num<totss;num++){
-            vector<int> ans;
-            for(int i=0;i<n;i++){
-                if((num&(1<<i))!=0){
-                    ans.push_back(nums[i]);
-                }
+        vector<vector<int>> ans;
+        for(int i=0;i<(1<<n);i++){
+            int num=i;
+            vector<int> tp;
+            for(int j=0;j<n;j++){
+                if((num&(1<<j))!=0){tp.push_back(nums[j]);}
             }
-            v.push_back(ans);
+            ans.push_back(tp);
         }
-        return v;
+        return ans;
     }
 };
