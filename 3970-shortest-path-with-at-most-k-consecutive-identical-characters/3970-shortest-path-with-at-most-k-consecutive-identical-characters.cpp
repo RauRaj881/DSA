049bc64ff2dd37f7b3d3059dec48e457b5e0e877
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-06-21 10:42:48 +0530

    Time: 280 ms (24.34%), Space: 324.5 MB (11.97%) - LeetHub

---
+++ 3970-shortest-path-with-at-most-k-consecutive-identical-characters/3970-shortest-path-with-at-most-k-consecutive-identical-characters.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int shortestPath(int n, vector<vector<int>>& ed, string lbl,int k){
+        vector<vector<pair<int,int>>> adj(n);
+        for(int i=0;i<ed.size();i++){
+            int u=ed[i][0];
+            int v=ed[i][1];
+            int w=ed[i][2];
+            adj[u].push_back({v,w});
+        }
+        vector<vector<long long>> dist(
+            n, vector<long long>(k + 1, LLONG_MAX)
+        );
+        set<vector<long long>> st;
+        st.insert({0,0,1});
+        dist[0][1]=0;
+        while(!st.empty()){
+            auto it=*st.begin();
+            int w=it[0],u=it[1],cnt=it[2];
+            st.erase(it);
+            //if (w != dist[u][cnt]) continue;
+            if(it[1]==n-1){return it[0];}
+            for(auto it2:adj[u]){
+                int cntp=cnt;
+                int v=it2.first;
+                int adw=it2.second;
+                if(lbl[v]==lbl[u]){cntp++;}
+                else{cntp=1;}
+                if(cntp<=k){
+                    long long nw = w + adw;
+                    if (nw < dist[v][cntp]){
+                        dist[v][cntp] = nw;
+                        st.insert({nw, v, cntp});
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+};
