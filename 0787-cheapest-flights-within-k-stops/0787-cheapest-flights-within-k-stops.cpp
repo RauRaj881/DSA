7a45d3599c9e58a7e24421ab6cb7778bb463ee6b
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-06-21 14:05:08 +0530

    Time: 15 ms (10.54%), Space: 20.1 MB (8.95%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -9,7 +9,9 @@ public:
             adj[u].push_back({v,w});
         }
         set<vector<int>> st;
+        vector<vector<int>> dist(n,vector<int>(k+2,INT_MAX));
         st.insert({0,src,0});
+        dist[src][0]=0;
         while(!st.empty()){
             auto it=*st.begin();
             int w=it[0];
@@ -20,7 +22,8 @@ public:
             for(auto it2:adj[u]){
                 int v=it2.first;
                 int adw=it2.second;
-                if(stp<=k){
+                if(stp<=k&&dist[v][stp+1]>w+adw){
+                    dist[v][stp+1]=w+adw;
                     st.insert({w+adw,v,stp+1});
                 }
             }
