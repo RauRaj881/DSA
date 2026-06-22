eeef4a6a0f731dd9e39c1e24e8d60bf5ce725f75
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-06-22 11:06:17 +0530

    Time: 10 ms (100%), Space: 277.9 MB (60%) - LeetHub

--- 3964-minimum-lights-to-illuminate-a-road/3964-minimum-lights-to-illuminate-a-road.cpp
+++ 3964-minimum-lights-to-illuminate-a-road/3964-minimum-lights-to-illuminate-a-road.cpp
@@ -2,36 +2,28 @@ class Solution {
 public:
     int minLights(vector<int>& lights){
         int n=lights.size();
-        vector<int> diff(n + 1, 0);
-
-        for (int i = 0; i < n; i++) {
-            if (lights[i] > 0) {
-                int l = max(0, i - lights[i]);
-                int r = min(n - 1, i + lights[i]);
-
-                diff[l]++;
-                if (r + 1 < n)
-                    diff[r + 1]--;
+        vector<int> df(n+1,0);
+        for(int i=0;i<n;i++){
+            int x=lights[i];
+            if(x>0){
+                int l=max(0,i-x);
+                int r=min(n-1,i+x);
+                df[l]++;
+                df[r+1]--;
             }
         }
-
-        int ans = 0;
-        int cur = 0;
-        int len = 0;
-
-        for (int i = 0; i < n; i++) {
-            cur += diff[i];
-
-            if (cur == 0) {
-                len++;
-            } else {
-                ans += (len + 2) / 3;
-                len = 0;
+        int ans=0;
+        int cr=0;
+        int len=0;
+        for(int i=0;i<n;i++){
+            cr+=df[i];
+            if(cr==0){len++;}
+            else{
+                ans+=(len+2)/3;
+                len=0;
             }
         }
-
-        ans += (len + 2) / 3;
-
+        ans+=(len+2)/3;
         return ans;
     }
 };
