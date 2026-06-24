3a52f1dfd05e9458639591c18e9ae17d176de686
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-06-24 19:15:50 +0530

    Time: 0 ms (100%), Space: 22.2 MB (40.81%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -4,13 +4,13 @@ public:
         vector<int> ans;
         stack<int> st;
         for(int i=0;i<ast.size();i++){
-            bool pos=true;
+            bool alive=true;
             while(!st.empty()&&st.top()>0&&ast[i]<0){
                 if(abs(ast[i])>st.top()){st.pop();}
-                else if(abs(ast[i])==st.top()){pos=false;st.pop();break;}
-                else{pos=false;break;}
+                else if(abs(ast[i])==st.top()){alive=false;st.pop();break;}
+                else{alive=false;break;}            
             }
-            if(pos){st.push(ast[i]);}
+            if(alive){st.push(ast[i]);}
         }
         while(!st.empty()){ans.push_back(st.top());st.pop();}
         reverse(ans.begin(),ans.end());
