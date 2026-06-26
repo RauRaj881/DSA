ba3435a8bb1a96e685135178005ad7bcc531eb48
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-06-26 22:29:30 +0530

    Time: 7 ms (6.64%), Space: 9.5 MB (27.87%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -5,10 +5,10 @@ public:
         unordered_map<char,char> mp1;
         unordered_map<char,char> mp2;
     for(int i=0;i<s.size();i++){
-        if(!mp1.count(s[i])&&!mp2.count(t[i])){mp1[s[i]]=t[i];mp2[t[i]]=s[i];}
-        else if(mp1.count(s[i])&&mp1[s[i]]!=t[i]){return false;}
+        if(mp1.count(s[i])&&mp1[s[i]]!=t[i]){return false;}
         else if(mp2.count(t[i])&&mp2[t[i]]!=s[i]){return false;}
-        }
+        else {mp1[s[i]]=t[i];mp2[t[i]]=s[i];}
+    }
         return true;
     }
 };
