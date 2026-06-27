005fc6b34b9a3c456cdfd602064dd7a27987c771
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-06-27 15:59:17 +0530

    Time: 71 ms (84.43%), Space: 96.9 MB (86.08%) - LeetHub

--- 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.cpp
+++ 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
-void dfs2(int node,int parent,vector<int> &ans,vector<vector<int>> &adj, vector<int> &s,int n){
+void dfs2(int node,int prt,vector<vector<int>> &adj,vector<int>&ans,vector<int>&s,int n){
     for(auto it:adj[node]){
-        if(it==parent){continue;}
-        ans[it]=ans[node]-2*s[it]+n;
-        dfs2(it,node,ans,adj,s,n);
+        if(it!=prt){
+            ans[it]=ans[node]+n-s[it]-s[it];
+            dfs2(it,node,adj,ans,s,n);
+        }
     }
 }
-void dfs(int node,int parent,vector<int> &ans,vector<vector<int>> &adj, vector<int> &s,int n){
+
+void dfs(int node,int prt,vector<vector<int>> &adj,vector<int>&ans,vector<int>&s,int n){
     for(auto it:adj[node]){
-        if(it==parent){continue;}
-        dfs(it,node,ans,adj,s,n);
-        s[node]+=s[it];
-        ans[node]+=ans[it]+s[it];
+        if(it!=prt){
+            dfs(it,node,adj,ans,s,n);
+            s[node]+=s[it];
+            ans[node]+=ans[it]+s[it];
+        }
     }
 }
-    vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
+    vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& ed){
         vector<vector<int>> adj(n);
-        for(int i=0;i<edges.size();i++){
-            int u=edges[i][0];
-            int v=edges[i][1];
+        for(int i=0;i<ed.size();i++){
+            int u=ed[i][0];
+            int v=ed[i][1];
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-        vector<int> ans(n,0);
         vector<int> s(n,1);
-        dfs(0,-1,ans,adj,s,n);
-        dfs2(0,-1,ans,adj,s,n);
+        vector<int> ans(n,0);
+        dfs(0,-1,adj,ans,s,n);
+        dfs2(0,-1,adj,ans,s,n);
         return ans;
     }
 };
