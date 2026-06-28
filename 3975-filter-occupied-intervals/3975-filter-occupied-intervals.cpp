5d588d496ac661f3d9d17012b576b5b4186a79e6
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-06-28 12:09:27 +0530

    Time: 101 ms (42.82%), Space: 193.7 MB (37.39%) - LeetHub

--- 3975-filter-occupied-intervals/3975-filter-occupied-intervals.cpp
+++ 3975-filter-occupied-intervals/3975-filter-occupied-intervals.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    static bool comp(vector<int>&a,vector<int>&b){
-        if(a[0]!=b[0]){return a[0]<b[0];}
-        return a[1]<b[1];
-    }
     vector<vector<int>> filterOccupiedIntervals(vector<vector<int>>& oci, int frst,int fred){
         int n=oci.size();
-        sort(oci.begin(),oci.end(),comp);
+        sort(oci.begin(),oci.end());
         int st=oci[0][0];
         int ed=oci[0][1];
         vector<vector<int>> mrg;
-        for (int i = 1; i < n; i++) {
-    if (oci[i][0] <= ed + 1) {
-        ed = max(ed, oci[i][1]);
-    } else {
+        for (int i = 1; i < n; i++){
+           if (oci[i][0] <= ed + 1){
+              ed = max(ed, oci[i][1]);
+           } 
+           else {
+              mrg.push_back({st, ed});
+              st = oci[i][0];
+              ed = oci[i][1];
+            }
+        }
         mrg.push_back({st, ed});
-        st = oci[i][0];
-        ed = oci[i][1];
-    }
-}
-mrg.push_back({st, ed});
         vector<vector<int>> ans;
 
         for (auto &v : mrg) {
