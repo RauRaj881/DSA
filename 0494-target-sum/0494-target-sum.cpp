c784e295489e9ab211573dbc53402a24ea13936f
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-06-29 22:07:25 +0530

    Time: 3 ms (85.63%), Space: 15.4 MB (52.95%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,13 +1,26 @@
 class Solution {
 public:
-int cnt=0;
-void f(int i,int sm,vector<int>& nums, int tar){
-    if(i==nums.size()){if(sm==tar){cnt++;}return;}
-    f(i+1,sm+nums[i],nums,tar);
-    f(i+1,sm-nums[i],nums,tar);
-}
     int findTargetSumWays(vector<int>& nums, int tar){
-        f(0,0,nums,tar);
-        return cnt;
+        int n=nums.size();
+        int sm=0;
+        for(auto it:nums){sm+=it;}
+        if((sm+tar)%2!=0||abs(tar)>sm){return 0;}
+        int tar2=(sm+tar)/2;
+        vector<vector<int>> dp(n,vector<int>(tar2+1,0));
+        for(int i=0;i<n;i++){
+            dp[i][0]=1;
+        }
+        if(nums[0]<=tar2){dp[0][nums[0]]++;}
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=tar2;j++){
+                int nt=dp[i-1][j];
+                int t=0;
+                if(nums[i]<=j){
+                    t=dp[i-1][j-nums[i]];
+                }
+                dp[i][j]=t+nt;
+            }
+        }
+        return dp[n-1][tar2];
     }
 };
