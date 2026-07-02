232f7fe348458136b44b2f3411ec884eb581b1dd
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-07-02 16:55:19 +0530

    Time: 0 ms (100%), Space: 8.5 MB (49.49%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     int mySqrt(int x){
-        return sqrt(x);
+        if(x<=1){return x;}
+        long long l=1,r=x;
+        long long ans=-1;
+        while(l<=r){
+            long long mid=(l+r)/2;
+            if(mid*mid==1LL*x){return mid;}
+            else if(mid*mid<1LL*x){ans=mid;l=mid+1;}
+            else{r=mid-1;}
+        }
+        return ans;
+        
     }
 };
