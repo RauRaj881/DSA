aea915addf7360dc267941bd4b6e3d2c94288db5
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-07-02 20:00:53 +0530

    Time: 127 ms (34.06%), Space: 21.4 MB (33.95%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        queue<pair<string,int>> q;
-        int x=1;
-        unordered_set<string> u;
-        for(int i=0;i<wordList.size();i++){
-            u.insert(wordList[i]);
-        }
-        if(u.find(endWord)==u.end()){return 0;}
-        q.push({beginWord,x});
+    int ladderLength(string bw, string ew, vector<string>& wl){
+        int n=wl.size();
+        int m=bw.size();
+        unordered_set<string> st;
+        for(auto it:wl){st.insert(it);}
+        if(!st.count(ew)){return 0;}
+        queue<string> q;
+        q.push(bw);
+        int level=0;
+        unordered_set<string> vis;
+        vis.insert(bw);
         while(!q.empty()){
-            auto temp=q.front();
-            string word=temp.first;
-            int level=temp.second;
-            q.pop();
-            for(int i=0;i<word.size();i++){
-                for(int j='a';j<='z';j++){
-                    string curr=word;
-                    curr[i]=char(j);
-                    if(u.find(curr)!=u.end()){
-                        q.push({curr,level+1});
-                        u.erase(curr);
-                    }
-                   // else{curr=word;}
-                    if(curr==endWord){return level+1;}
+            level++;
+            int sz=q.size();
+            for(int k=0;k<sz;k++){
+                string cr=q.front();
+                q.pop();
+            for(int i=0;i<m;i++){
+                for(int j=0;j<26;j++){
+                    char nw=j+'a';
+                    string tp=cr;
+                    tp[i]=nw;
+                    if(tp==ew){return level+1;}
+                    if(st.count(tp)&&!vis.count(tp)){q.push(tp);vis.insert(tp);}
                 }
+            }
             }
         }
         return 0;
+        
     }
 };
