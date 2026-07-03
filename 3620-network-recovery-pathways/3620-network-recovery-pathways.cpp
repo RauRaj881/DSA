2c64cb2aad9483525cf1483da68b8d3e3f44a248
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-07-03 23:49:38 +0530

    Time: 1345 ms (13.83%), Space: 534.9 MB (5.13%) - LeetHub

--- 3620-network-recovery-pathways/3620-network-recovery-pathways.cpp
+++ 3620-network-recovery-pathways/3620-network-recovery-pathways.cpp
@@ -1,46 +1,41 @@
 class Solution {
 public:
-bool ispossible(long long mid,vector<vector<pair<long long,long long>>> &adj,long long k,int n){
-    set<pair<long long,long long>> s;
-    s.insert({0,0});
-    vector<long long> mnd(n,k+1);
-    while(!s.empty()){
-        auto it=*s.begin();
-        long long dist=it.first;
-        long long nd=it.second;
-        s.erase(it);
-        if(nd==n-1){return true;}
-        for(auto ig:adj[nd]){
-            long long v=ig.first;
-            long long w=ig.second;
-            if(dist+w<=k&&w>=mid&&dist+w<mnd[v]){
-                s.erase({mnd[v], v});
-                mnd[v]=dist+w;
-                s.insert({dist+w,v});
+bool ispossible(long long mid,vector<bool>& onl,vector<vector<pair<int,int>>> &adj,long long k){
+    int n=onl.size();
+    set<vector<long long>> st;
+    st.insert({0,0});
+    vector<long long> dst(n,k+1);
+    dst[0]=0;
+    while(!st.empty()){
+        vector<long long> it=*st.begin();
+        long long w=it[0],u=it[1];
+        st.erase(st.begin());
+        if(u==n-1){return true;}
+        for(auto it2:adj[u]){
+            long long v=it2.first;
+            long long adw=it2.second;
+            if(w+adw<=k&&adw>=mid&&dst[v]>w+adw&&onl[v]){
+                st.erase({dst[v],v});
+                st.insert({w+adw,v});
+                dst[v]=w+adw;
             }
         }
     }
     return false;
 }
-    int findMaxPathScore(vector<vector<int>>& edges, vector<bool>& online, long long k) {
-        int n=online.size();
-        vector<vector<pair<long long,long long>>> adj(n);
-        for(int i=0;i<edges.size();i++){
-            int u=edges[i][0];
-            int v=edges[i][1];
-            int w=edges[i][2];
-            if(online[v]&&online[u])adj[u].push_back({v,w});
+    int findMaxPathScore(vector<vector<int>>& ed,vector<bool>& onl,long long k){
+        int n=onl.size();
+        vector<vector<pair<int,int>>> adj(n);
+        for(int i=0;i<ed.size();i++){
+            int u=ed[i][0],v=ed[i][1],w=ed[i][2];
+            if(onl[v]&&onl[u])adj[u].push_back({v,w});
         }
+        int ans=-1;
         long long low=0,high=1e9;
-        long long ans=-1;
         while(low<=high){
             long long mid=(low+high)/2;
-            if(ispossible(mid,adj,k,n)){
-                ans=mid;low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
+            if(ispossible(mid,onl,adj,k)){ans=mid;low=mid+1;}
+            else{high=mid-1;}
         }
         return ans;
     }
