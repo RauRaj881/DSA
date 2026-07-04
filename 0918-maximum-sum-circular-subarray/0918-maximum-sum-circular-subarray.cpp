46a5c9408cdd929ef5913b60089ef248b9afe0b7
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-07-04 12:16:03 +0530

    Time: 0 ms (100%), Space: 43.6 MB (48.53%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
@@ -5,9 +5,7 @@ public:
         int mx=INT_MIN;
         int sm=0;
         int tot=0;
-        int cntn=0;
         for(int i=0;i<n;i++){
-            if(nums[i]<0){cntn++;}
             tot+=nums[i];
             sm+=nums[i];
             mx=max(mx,sm);
@@ -15,14 +13,12 @@ public:
         }
         int mn=0;
         int sm2=0;
-        int mx2=INT_MIN;
         for(int i=0;i<n;i++){
-            mx2=max(mx2,nums[i]);
             sm2+=nums[i];
             mn=min(mn,sm2);
             if(sm2>0){sm2=0;}
         }
-        if(cntn==n){return mx2;}
+        if(mx<0){return mx;}
         return max(mx,tot-mn);
     }
 };
