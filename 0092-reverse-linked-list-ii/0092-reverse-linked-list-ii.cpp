6c001e7c2f8a156e4fce599065d9133230440489
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-07-04 13:09:40 +0530

    Time: 1 ms (1.27%), Space: 11.3 MB (38.57%) - LeetHub

---
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int lt,int rt){
+        if(lt==rt){return head;}
+        ListNode* left=nullptr;
+        ListNode* right=nullptr;
+        int cnt=1;
+        ListNode* tx=head;
+        while(tx!=nullptr){if(cnt==lt){left=tx;}if(cnt==rt){right=tx;}tx=tx->next;cnt++;}
+        ListNode* nd2=right->next;
+        ListNode* cr=head;
+        ListNode* nd1;
+        if(left==head){nd1=nullptr;}
+        else{
+            while(cr->next!=left){
+                cr=cr->next;
+            }
+            nd1=cr;
+        }
+        ListNode* cr1=left;
+        ListNode* cr2=left->next;
+        while(cr2!=nd2){
+            ListNode* nt=cr2->next;
+            cr2->next=cr1;
+            cr1=cr2;
+            cr2=nt;
+        }
+        left->next = nd2;
+
+        if(nd1)
+        nd1->next = cr1;
+        else
+        head = cr1;
+        return head;
+    }
+};
