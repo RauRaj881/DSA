6d1b930e9f0133ce5007a74b7377dc9ab81dc672
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-07-05 12:16:52 +0530

    Time: 12 ms (84.46%), Space: 38.9 MB (63.58%) - LeetHub

--- 3983-subsequence-after-one-replacement/3983-subsequence-after-one-replacement.cpp
+++ 3983-subsequence-after-one-replacement/3983-subsequence-after-one-replacement.cpp
@@ -1,43 +1,32 @@
 class Solution {
 public:
     bool canMakeSubsequence(string s, string t){
-        int n1 = s.size();
-        int n2 = t.size();
-        
-        if (n1 > n2) return false;
-        vector<int> left(n1, 1e9); 
-        int j = 0;
-        for (int i = 0; i < n1; i++) {
-            while (j < n2 && t[j] != s[i]) j++;
-            if (j < n2) {
-                left[i] = j;
-                j++; 
-            } else {
-                break; 
-            }
+        int n1=s.size();
+        int n2=t.size();
+        if(n1>n2){return false;}
+        vector<int> prm(n1,-1);
+        vector<int> sfm(n1,n2);
+        int i=0,j=0;
+        while(i<n1&&j<n2){
+            if(s[i]==t[j]){prm[i]=j;i++;j++;}
+            else{j++;}
         }
-        if (left[n1 - 1] != 1e9) return true;
-        vector<int> right(n1, -1e9);
-        j = n2 - 1;
-        for (int i = n1 - 1; i >= 0; i--) {
-            while (j >= 0 && t[j] != s[i]) j--;
-            if (j >= 0) {
-                right[i] = j;
-                j--;
-            } else {
-                break;
-            }
+        if(i==n1){return true;}
+        i=n1-1,j=n2-1;
+        while(i>=0&&j>=0){
+            if(s[i]==t[j]){sfm[i]=j;i--;j--;}
+            else{j--;}
         }
-        for (int k = 0; k < n1; k++) {
-            int l_idx = (k == 0) ? -1 : left[k - 1];
-            int r_idx = (k == n1 - 1) ? n2 : right[k + 1];
-            //if (l_idx != 1e9 && r_idx != -1e9) {
-                if (r_idx - l_idx >= 2) {
-                    return true;
-                }
-            //}
+        if(i==-1){return true;}
+        for(int i=0;i<n1;i++){
+            int idx1,idx2;
+            if(i==0){idx1=-1;}
+            else{if(prm[i-1]!=-1){idx1=prm[i-1];}else{continue;}}
+            if(i==n1-1){idx2=n2;}
+            else{if(sfm[i+1]!=n2){idx2=sfm[i+1];}else{continue;}}
+            if(idx2-idx1>=2){return true;}
         }
-
         return false;
+        
     }
 };
