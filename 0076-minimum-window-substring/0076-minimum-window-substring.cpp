752bf02a17e2ed2bf3e9d433b04f6e15c8ef8f6f
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-07-08 10:47:39 +0530

    Time: 15 ms (36.03%), Space: 11.5 MB (65.34%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    string minWindow(string s, string t){
-        int n=s.size();
-        int m=t.size();
-        unordered_map<char,int> mp;
-        for(auto it:t){mp[it]++;}
-        int sz=mp.size();
-        //int cnt=0;
-        int cr=0;
-        int l=0;
-        int len=1e6,st=-1;
-        unordered_map<char,int> mp2;
-        for(int r=0;r<s.size();r++){
-            mp2[s[r]]++;
-            if(mp2[s[r]]==mp[s[r]]){cr++;}
-            while(cr>=sz&&l<=r){
-                if(r-l+1<len){len=r-l+1;st=l;}
-                mp2[s[l]]--;
-                if(mp2[s[l]]+1==mp[s[l]]){cr--;}
+    string minWindow(string s, string t) {
+        int m=s.length();
+        int n=t.length();
+        map<char,int> mp;
+        for(int i=0;i<n;i++){
+            mp[t[i]]++;
+        }
+        int cnt=0;
+        int l=0,r=0;
+        int startl=-1,minr=0;
+        int minlength=m;
+        while(r<m){
+            if(mp[s[r]]>0){cnt++;};
+            mp[s[r]]--;
+            while(l<=r&&cnt>=n){
+                if(r-l+1<=minlength){minlength=r-l+1;startl=l;}
+                mp[s[l]]++;
+                if(mp[s[l]]>0){cnt--;}
                 l++;
             }
+            r++;
         }
-        if(st==-1){return "";}
-        return s.substr(st,len);
+        if(startl==-1){return s.substr(0,0);}
+        return s.substr(startl,minlength); 
     }
 };
