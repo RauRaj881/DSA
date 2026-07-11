5351fa2d3e136cddec9010ce14d72791808ca97c
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-07-11 21:46:47 +0530

    Time: 32 ms (78.83%), Space: 32.1 MB (78.07%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.cpp
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
-    bool isNStraightHand(vector<int>& hand, int sz){
-        int n=hand.size();
+    bool isNStraightHand(vector<int>& hnd,int sz){
+        int n=hnd.size();
         if(n%sz!=0){return false;}
         map<int,int> mp;
-        for(auto it:hand){
-            mp[it]++;
-        }
+        for(auto it:hnd){mp[it]++;}
         while(!mp.empty()){
             int st=mp.begin()->first;
             for(int i=0;i<sz;i++){
