62caf1d8f7cbcb68bf747a9dd2a4491163dcdd45
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-07-11 21:19:44 +0530

    Time: 2 ms (58%), Space: 17.9 MB (62.17%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,41 +1,17 @@
-/*class Solution {
-public:
-    vector<int> topKFrequent(vector<int>& nums, int k) {
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        vector<pair<int,int>> v;
-        int cnt=1;
-        for(int i=1;i<n;i++){
-            if(nums[i]==nums[i-1]){cnt++;}
-            else{
-                v.push_back({cnt,nums[i-1]});
-                cnt=1;
-            }
-        }
-        v.push_back({cnt,nums[n-1]});
-        sort(v.rbegin(),v.rend());
-        vector<int> ans(k);
-        for(int i=0;i<k;i++){
-            ans[i]=v[i].second;
-        }
-        return ans;
-    }
-};*/
 class Solution {
 public:
-    vector<int> topKFrequent(vector<int>& nums, int k) {
+    vector<int> topKFrequent(vector<int>& nums, int k){
         int n=nums.size();
         unordered_map<int,int> mp;
         for(auto it:nums){mp[it]++;}
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        vector<pair<int,int>> v;
         for(auto it:mp){
-            pq.push({it.second,it.first});
-            if(pq.size()>k){pq.pop();}
+            v.push_back({it.second,it.first});
         }
-        vector<int> ans(k);
+        sort(v.begin(),v.end(),greater<pair<int,int>>());
+        vector<int> ans;
         for(int i=0;i<k;i++){
-            ans[i]=pq.top().second;
-            pq.pop();
+            ans.push_back(v[i].second);
         }
         return ans;
     }
