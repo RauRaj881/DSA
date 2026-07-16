dc0b7fd18ff3e6f2146bdf22827e9cd3c091b8c0
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-07-16 23:54:08 +0530

    Time: 109 ms (13.34%), Space: 50.8 MB (44.87%) - LeetHub

--- 0480-sliding-window-median/0480-sliding-window-median.cpp
+++ 0480-sliding-window-median/0480-sliding-window-median.cpp
@@ -3,12 +3,12 @@ public:
     vector<double> medianSlidingWindow(vector<int>& nums, int k){
         int n=nums.size();
         vector<double> ans;
-        auto cmp = [&](int a, int b) {
-            if (nums[a] == nums[b]) return b > a;
-            return nums[a] < nums[b];
+        auto cmp=[&](int i,int j){
+            if(nums[i]==nums[j]){return i>j;}
+            return nums[i]<nums[j];
         };
 
-        multiset<int, decltype(cmp)> ms1(cmp), ms2(cmp);
+        multiset<int,decltype(cmp)> ms1(cmp),ms2(cmp);
         for(int i=0;i<n;i++){
             if(i>=k){if(ms1.count(i-k)){ms1.erase(i-k);}else{ms2.erase(i-k);}}
             if(ms1.empty()||nums[i]<nums[*ms1.rbegin()]){
