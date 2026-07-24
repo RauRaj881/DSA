a9e288b28f4511a4622306af3694827c9bc544e1
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-07-24 09:02:41 +0530

    Time: 38 ms (25.44%), Space: 31.6 MB (38.52%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,41 @@
+//tabulation
+class Solution {
+public:
+    int minInsertions(string s) {
+        string t=s;
+        reverse(s.begin(),s.end());
+        int n=s.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s[i-1]==t[j-1]){dp[i][j]=1+dp[i-1][j-1];}
+                else{dp[i][j]=max(dp[i-1][j],dp[i][j-1]);}
+            }
+        }
+        return n-dp[n][n];
+    }
+};
+/*class Solution {
+public:
+    int minInsertions(string s) {
+        string t=s;
+        reverse(s.begin(),s.end());
+        int n=s.size();
+        int maxno=0;
+        vector<int> prev(n+1,0);
+        for(int i=1;i<=n;i++){
+            vector<int> curr(n+1,0);
+            for(int j=1;j<=n;j++){
+                int nottake=max(prev[j],curr[j-1]);
+                int take=0;
+                if(s[i-1]==t[j-1]){
+                    take=1+prev[j-1];
+                }
+                curr[j]=max(take,nottake);
+                maxno=max(maxno,curr[j]);
+            }
+            prev=curr;
+        }
+        return n-maxno;
+    }
+};*/
