c128a1d0a7f27dd2305b1c766c24a97ed07943c3
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-07-24 11:48:37 +0530

    Time: 203 ms (36.9%), Space: 132.7 MB (8.95%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -3,8 +3,10 @@ public:
     int countPrimes(int n){
         vector<int> v(n+1,-1);
         for(long long i=2;i<=n;i++){
+            if(v[i]==-1){
             for(long long j=i*i;j<=n;j+=i){
                 v[j]=1;
+            }
             }
         }
         int cnt=0;
