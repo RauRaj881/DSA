dd9511147a1ee98289e2ac6afba1c5801ca4d4a2
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-07-24 11:45:38 +0530

    Time: 403 ms (7.1%), Space: 132.5 MB (15.54%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-    int countPrimes(int n) {
-        vector<int> prime(n,1);
+    int countPrimes(int n){
+        vector<int> v(n+1,-1);
+        for(long long i=2;i<=n;i++){
+            for(long long j=i*i;j<=n;j+=i){
+                v[j]=1;
+            }
+        }
         int cnt=0;
-        for(long long i=2;i<n;i++){
-            if(prime[i]==1){
-                cnt++;
-                for(long long j=i*i;j<n;j+=i){
-                prime[j]=0;
-            }
-            }
+        for(int i=2;i<n;i++){
+            if(v[i]==-1){cnt++;}
         }
         return cnt;
     }
 };
-//TC=O(nlog(logn)) just remember it.(Sieve of Eratosthenes)
-//SC=O(n)
