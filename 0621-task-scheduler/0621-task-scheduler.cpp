37509a40b7b951f98c9c9fbf483e658f955e1724
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-07-24 13:29:40 +0530

    Time: 87 ms (5.89%), Space: 63.5 MB (5%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
-    int leastInterval(vector<char>& tsk, int n){
-        int m=tsk.size();
-        vector<int> fr(26,0);
-        for(auto it:tsk){fr[it-'A']++;}
+    int leastInterval(vector<char>& tasks, int n){
         priority_queue<pair<int,int>> pq;
-        for(int i=0;i<26;i++){if(fr[i]>0){pq.push({fr[i],i});}}
-        queue<pair<int,int>> cool;
-        int time=0;
-        while(!pq.empty()||!cool.empty()){
-            while(!cool.empty()&&cool.front().second<=time){
-                int i=cool.front().first;
-                cool.pop();
-                pq.push({fr[i],i});
+        queue<vector<int>> q;
+        vector<int> v(26,0);
+        for(auto it:tasks){v[it-'A']++;}
+        for(int i=0;i<26;i++){
+            if(v[i]>0){pq.push({v[i],i});}
+        }
+        int tm=0;
+        while(!pq.empty()||!q.empty()){
+            while(!q.empty()&&tm-q.front()[2]>n){
+                pq.push({q.front()[1],q.front()[0]});
+                q.pop();
             }
             if(!pq.empty()){
-                int i=pq.top().second;
+                int fr=pq.top().first;
+                int ch=pq.top().second;
+                fr--;
                 pq.pop();
-                fr[i]--;
-                if(fr[i]>0){cool.push({i,time+n+1});}
+                if(fr>0){
+                    q.push({ch,fr,tm});
+                }
             }
-            time++;
+            tm++;
         }
-        return time;
+        return tm;
     }
 };
