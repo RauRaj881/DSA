02e2d0eae7c490a92ff490b106a201066aa2f929
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-07-24 09:07:53 +0530

    Time: 31 ms (63.45%), Space: 31.3 MB (74.13%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -5,14 +5,17 @@ public:
         string t=s;
         reverse(s.begin(),s.end());
         int n=s.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                if(s[i-1]==t[j-1]){dp[i][j]=1+dp[i-1][j-1];}
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        if(s[0]==t[0]){dp[0][0]=1;}
+    for(int j=1;j<n;j++){if(s[0]==t[j]){dp[0][j]=1;}else{dp[0][j]=dp[0][j-1];}}
+    for(int j=1;j<n;j++){if(s[j]==t[0]){dp[j][0]=1;}else{dp[j][0]=dp[j-1][0];}}
+        for(int i=1;i<n;i++){
+            for(int j=1;j<n;j++){
+                if(s[i]==t[j]){dp[i][j]=1+dp[i-1][j-1];}
                 else{dp[i][j]=max(dp[i-1][j],dp[i][j-1]);}
             }
         }
-        return n-dp[n][n];
+        return n-dp[n-1][n-1];
     }
 };
 /*class Solution {
