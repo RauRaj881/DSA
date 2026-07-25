f511abc6855b6a4c5ebaf2da91e6f9efc9aa2ba8
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-07-25 18:29:11 +0530

    Time: 381 ms (60.34%), Space: 73.2 MB (95.69%) - LeetHub

--- 2035-partition-array-into-two-arrays-to-minimize-sum-difference/2035-partition-array-into-two-arrays-to-minimize-sum-difference.cpp
+++ 2035-partition-array-into-two-arrays-to-minimize-sum-difference/2035-partition-array-into-two-arrays-to-minimize-sum-difference.cpp
@@ -1,42 +1,44 @@
 class Solution {
 public:
-void storesm(vector<int>&nums,vector<vector<int>>&left,vector<vector<int>>&right,int tot){
+void f(vector<vector<int>> &lt,vector<vector<int>>& rt,vector<int>& nums,int tot){
     int n=nums.size();
-    for(int i=0;i<=(1<<(n/2));i++){
+    for(int i=0;i<(1<<(n/2));i++){
         int sz=0;
         int sm1=0,sm2=0;
         for(int j=0;j<n/2;j++){
-            if((i&(1<<j))!=0){sz++;sm1+=nums[j];sm2+=nums[j+n/2];}
+            if((i&(1<<j))!=0){sz++;sm1+=nums[j];sm2+=nums[n/2+j];}
         }
-        left[sz].push_back(sm1);
-        right[sz].push_back(sm2);
+        lt[sz].push_back(sm1);
+        rt[sz].push_back(sm2);
     }
 }
-    int minimumDifference(vector<int>& nums) {
+    int minimumDifference(vector<int>& nums){
         int n=nums.size();
         int tot=0;
-        for(auto it:nums){
-            tot+=it;
-        }
-        vector<vector<int>> left(n/2+1);
-        vector<vector<int>> right(n/2+1);
-        storesm(nums,left,right,tot);
+        for(auto it:nums){tot+=it;}
+        vector<vector<int>> lt(n/2+1);
+        vector<vector<int>> rt(n/2+1);
+        f(lt,rt,nums,tot);
         for(int i=0;i<=n/2;i++){
-            sort(right[i].begin(),right[i].end());
+            sort(rt[i].begin(),rt[i].end());
         }
-        int mn=1e9;
+        int mn=INT_MAX;
         for(int sz=0;sz<=n/2;sz++){
-            for(auto it2:left[sz]){
-                auto it3=lower_bound(right[n/2-sz].begin(),right[n/2-sz].end(),(tot-2*it2)/2);
-                if(it3!=right[n/2-sz].end()){
-                    int idx=it3-right[n/2-sz].begin();
-                    int val=right[n/2-sz][idx];
-                    mn=min(mn,abs(tot-2*(it2+val)));
+            for(auto it:lt[sz]){
+                auto it2=lower_bound(rt[n/2-sz].begin(),rt[n/2-sz].end(),(tot-2*it)/2);
+                if(it2!=rt[n/2-sz].end()){
+                    int idx=it2-rt[n/2-sz].begin();
+                    int sm1=it,sm2=rt[n/2-sz][idx];
+                    int sb1=sm1+sm2;
+                    int sb2=tot-sb1;
+                    mn=min(mn,abs(sb1-sb2));
                 }
-                if(it3!=right[n/2-sz].begin()){
-                    int idx=it3-right[n/2-sz].begin()-1;
-                    int val=right[n/2-sz][idx];
-                    mn=min(mn,abs(tot-2*(it2+val)));
+                if(it2!=rt[n/2-sz].begin()){
+                    int idx=it2-rt[n/2-sz].begin()-1;
+                    int sm1=it,sm2=rt[n/2-sz][idx];
+                    int sb1=sm1+sm2;
+                    int sb2=tot-sb1;
+                    mn=min(mn,abs(sb1-sb2));
                 }
             }
         }
