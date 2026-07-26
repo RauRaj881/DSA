8277d0073e953e473675ed7de812538f9ee7a339
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-07-27 01:17:10 +0530

    Time: 99 ms (60.47%), Space: 103 MB (61.75%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,77 +1,14 @@
-//memoization
-/*class Solution {
-public:
-int ss(int index,int buy,vector<int>& prices,vector<vector<int>> &dp,int n,int fee){
-    if(index==n){return 0;}
-    if(dp[index][buy]!=-1){return dp[index][buy];}
-    if(buy==0){
-        dp[index][buy]=max(-prices[index]+ss(index+1,1-buy,prices,dp,n,fee),ss(index+1,buy,prices,dp,n,fee));
-    }
-    else{
-        dp[index][buy]=max(prices[index]-fee+ss(index+1,1-buy,prices,dp,n,fee),ss(index+1,buy,prices,dp,n,fee));
-    }
-    return dp[index][buy];
-}
-    int maxProfit(vector<int>& prices, int fee) {
-         int n=prices.size();
-        vector<vector<int>> dp(n,vector<int>(2,-1));
-        return ss(0,0,prices,dp,n,fee);  
-    }
-};*/
-//tabulation 1
-/*class Solution {
-public:
-    int maxProfit(vector<int>& prices, int fee) {
-        int n=prices.size();
-        vector<vector<int>> dp(n+1,vector<int>(2,0));
-        for(int i=n-1;i>=0;i--){
-            for(int buy=0;buy<=1;buy++){
-                 if(buy==0){
-               dp[i][buy]=max(-prices[i]+dp[i+1][1-buy],dp[i+1][buy]);
-    }
-                 else{
-                 dp[i][buy]=max(-fee+prices[i]+dp[i+1][1-buy],dp[i+1][buy]);
-    }        
-            }
-        }
-        return dp[0][0];
-    }
-};*/
-//space optimization
-/*class Solution {
-public:
-    int maxProfit(vector<int>& prices, int fee) {
-         int n=prices.size();
-        vector<int> prev(2,0);
-        for(int i=n-1;i>=0;i--){
-             vector<int> curr(2);
-            for(int buy=0;buy<=1;buy++){
-                 if(buy==0){
-               curr[buy]=max(-prices[i]+prev[1-buy],prev[buy]);
-    }
-                 else{
-                 curr[buy]=max(-fee+prices[i]+prev[1-buy],prev[buy]);
-    }        
-            }
-            prev=curr;
-        }
-        return prev[0]; 
-    }
-};*/
-// tabulation 2
 class Solution {
 public:
-    int maxProfit(vector<int>& prices, int fee) {
-        int n = prices.size();
-        if (n == 0) return 0;
-        vector<vector<int>> dp(n, vector<int>(2, 0));
-        dp[0][0] = 0;
-        dp[0][1] = -prices[0];
-        for (int i = 1; i < n; i++) {
-            dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i]-fee);
-            dp[i][1] = max(dp[i-1][1], dp[i-1][0]-prices[i]);
+    int maxProfit(vector<int>& p,int fee){
+        int n=p.size();
+        vector<vector<int>> dp(n,vector<int>(2,-1e9));
+        dp[0][0]=0;
+        dp[0][1]=-p[0];
+        for(int i=1;i<n;i++){
+            dp[i][0]=max(dp[i-1][0],dp[i-1][1]+p[i]-fee);
+            dp[i][1]=max(dp[i-1][1],dp[i-1][0]-p[i]);
         }
         return dp[n-1][0];
-        
     }
 };
