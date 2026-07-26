4c10aba1379c1bc35aebe767a1f82118bb96933b
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-07-27 00:43:00 +0530

    Time: 122 ms (79.5%), Space: 142.4 MB (57.81%) - LeetHub

--- 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.cpp
+++ 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.cpp
@@ -1,19 +1,31 @@
+#define ll long long
 class Solution {
 public:
-    long long maximumProfit(vector<int>& prices, int k) {
-    long long n=prices.size();
-    vector<vector<vector<long long>>> dp(n,vector<vector<long long>>(3,vector<long long>(k+1,0)));
-        for(int t=1;t<=k;t++){
-            dp[0][1][t]=-prices[0];
-            dp[0][2][t]=prices[0];
-        }
+    long long maximumProfit(vector<int>& p, int k){
+        int n=p.size();
+        vector<vector<vector<ll>>> dp(n,vector<vector<ll>>(3,vector<ll>(k+1,-1e17)));
+        dp[0][0][0] = 0;
+        dp[0][1][0]=-p[0];
+        dp[0][2][0]=p[0];
         for(int i=1;i<n;i++){
-            for(int t=1;t<=k;t++){
-                dp[i][0][t]=max(dp[i-1][0][t],max(dp[i-1][1][t]+prices[i],dp[i-1][2][t]-prices[i]));
-                dp[i][1][t]=max(dp[i-1][1][t],dp[i-1][0][t-1]-prices[i]);
-                dp[i][2][t]=max(dp[i-1][2][t],dp[i-1][0][t-1]+prices[i]);
+            for(int t=0;t<=k;t++){
+                if(t==0){
+                    dp[i][0][0]=dp[i-1][0][0];
+                    dp[i][1][0]=max(dp[i-1][1][0],dp[i-1][0][0]-p[i]);
+                    dp[i][2][0]=max(dp[i-1][2][0],dp[i-1][0][0]+p[i]);
+                }
+                else{
+                    dp[i][0][t]=max(dp[i-1][0][t],max(dp[i-1][1][t-1]+p[i],dp[i-1][2][t-1]-p[i]));
+                    dp[i][1][t]=max(dp[i-1][1][t],dp[i-1][0][t]-p[i]);
+                    dp[i][2][t]=max(dp[i-1][2][t],dp[i-1][0][t]+p[i]);
+                }
             }
         }
-        return dp[n-1][0][k];
+        long long ans=0;
+        for(int i=0;i<=k;i++){
+            ans=max(ans,dp[n-1][0][i]);
+        }
+        return ans;
+        
     }
 };
