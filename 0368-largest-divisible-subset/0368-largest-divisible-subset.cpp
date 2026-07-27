1744df8f892efd1754f66a7521b4e6841c11a0b3
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-07-27 12:30:03 +0530

    Time: 3 ms (99.66%), Space: 12.6 MB (78.42%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -3,33 +3,25 @@ public:
     vector<int> largestDivisibleSubset(vector<int>& nums){
         sort(nums.begin(),nums.end());
         int n=nums.size();
-        vector<int> dp(n,1);
-        vector<int> parent(n);
+        vector<pair<int,int>> dp(n);
+        for(int i=0;i<n;i++){dp[i]={1,i};}
         for(int i=0;i<n;i++){
-            parent[i]=i;
             for(int j=0;j<i;j++){
-                if(nums[i]%nums[j]==0&&dp[j]+1>dp[i]){
-                    dp[i]=dp[j]+1;
-                    parent[i]=j;
+                if(nums[i]%nums[j]==0){
+                    if(dp[j].first+1>=dp[i].first){dp[i]={dp[j].first+1,j};}
                 }
             }
         }
-
-        int mx=1;
-        int ltidx=0;
-        for(int i=0;i<n;i++){
-            if(dp[i]>mx){
-                mx=dp[i];
-                ltidx=i;
-            }
-        }
-
+        int mx=1,ed=-1;
+        for(int i=0;i<n;i++){if(dp[i].first>=mx){mx=dp[i].first;ed=i;}}
         vector<int> ans;
-        while(parent[ltidx]!=ltidx){
-            ans.push_back(nums[ltidx]);
-            ltidx=parent[ltidx];
+        int cr=ed;
+        while(dp[cr].second!=cr){
+            ans.push_back(nums[cr]);
+            cr=dp[cr].second;
         }
-        ans.push_back(nums[ltidx]);
+        ans.push_back(nums[cr]);
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
