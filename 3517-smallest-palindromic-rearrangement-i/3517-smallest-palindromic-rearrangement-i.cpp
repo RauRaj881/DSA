1e1578be6936a645352da4e14e7d54636414fa51
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-07-29 18:45:51 +0530

    Time: 12 ms (97.4%), Space: 60.9 MB (78.74%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -10,13 +10,9 @@ public:
         while(i<=n/2){
             for(int j=k;j<26;j++){
                 if(v[j]>=2){ans[i]=j+'a';ans[n-i-1]=j+'a';v[j]-=2;k=j;break;}
+                else if(v[j]==1){ans[n/2]=j+'a';}
             }
             i++;
-        }
-        if(n%2==1){
-            for(int i=0;i<26;i++){
-                if(v[i]==1){ans[n/2]=i+'a';}
-            }
         }
         return ans;
     }
