3ca4ce08205a9823a8945868c311b11f9630adff
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-07-29 23:09:24 +0530

    Time: 0 ms (100%), Space: 31.4 MB (97.7%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     int maximumProduct(vector<int>& nums){
         int n=nums.size();
-        if(n==3){return nums[n-1]*nums[n-2]*nums[n-3];}
-        vector<int> neg;
-        vector<int> pos;
-        for(auto it:nums){if(it<=0){neg.push_back(it);}else{pos.push_back(it);}}
-        sort(neg.begin(),neg.end());
-        sort(pos.begin(),pos.end());
-        int sz1=neg.size(),sz2=pos.size();
-        if(neg.size()<=1){return pos[sz2-1]*pos[sz2-2]*pos[sz2-3];}
-        if(pos.size()==0){return neg[sz1-1]*neg[sz1-2]*neg[sz1-3];}
-        int mx1=neg[0]*neg[1];
-        int mx2=-1;
-        if(pos.size()>=3){mx2=pos[sz2-2]*pos[sz2-3];}
-        return max(mx1,mx2)*pos[sz2-1];
+        int mx1=INT_MIN,mx2=INT_MIN,mx3=INT_MIN;
+        int mn1=INT_MAX,mn2=INT_MAX;
+        for(auto it:nums){
+            if(it>mx1){mx3=mx2;mx2=mx1;mx1=it;}
+            else if(it>mx2){mx3=mx2;mx2=it;}
+            else if(it>mx3){mx3=it;}
+
+            if(it<mn1){mn2=mn1;mn1=it;}
+            else if(it<mn2){mn2=it;}
+        }
+
+        return max(mx1*mx2*mx3,mx1*mn1*mn2);
     }
 };
