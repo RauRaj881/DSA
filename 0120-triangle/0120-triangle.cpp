369d74849c8b77986d9198de6dca24c3ca2fb760
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-07-30 13:19:17 +0530

    Time: 0 ms (100%), Space: 13 MB (65.19%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -3,20 +3,22 @@ public:
     int minimumTotal(vector<vector<int>>& tri){
         int m=tri.size();
         int n=tri[m-1].size();
-        vector<vector<int>> dp(m,vector<int>(n,INT_MAX));
-        dp[0][0]=tri[0][0];
+        vector<int> prv(n);
+        prv[0]=tri[0][0];
         for(int i=1;i<m;i++){
+            vector<int> cur(n);
             for(int j=0;j<=i;j++){
                 int up=INT_MAX;
                 int lt=INT_MAX;
-                if(j>0){lt=dp[i-1][j-1];}
-                if(j<i){up=dp[i-1][j];}
-                dp[i][j]=tri[i][j]+min(lt,up);
+                if(j>0){lt=prv[j-1];}
+                if(j<i){up=prv[j];}
+                cur[j]=tri[i][j]+min(lt,up);
             }
+            prv=cur;
         }
         int ans=INT_MAX;
         for(int j=0;j<=m-1;j++){
-            ans=min(ans,dp[m-1][j]);
+            ans=min(ans,prv[j]);
         }
         return ans;
     }
