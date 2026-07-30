f01fd57d3451d35535884ca6687a332767b91247
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-07-30 10:04:38 +0530

    Time: 0 ms (100%), Space: 16.2 MB (62.38%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,60 +1,20 @@
-//memoization
-/*class Solution {
+class Solution {
 public:
-int minps(int x,int y,vector<vector<int>>& grid,vector<vector<int>> &dp){
-    if(x==0 &&y==0){return grid[0][0];}
-    if(x<0||y<0){return 1e9;}
-    if(dp[x][y]!=-1){return dp[x][y];}
-    int up=minps(x-1,y,grid,dp)+grid[x][y];
-    int left=minps(x,y-1,grid,dp)+grid[x][y];
-    return dp[x][y]=min(up,left);
-}
-    int minPathSum(vector<vector<int>>& grid) {
+    int minPathSum(vector<vector<int>>& grid){
         int m=grid.size();
         int n=grid[0].size();
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return minps(m-1,n-1,grid,dp);
-    }
-};*/
-//tabulation
-/*class Solution {
-public:
-    int minPathSum(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        vector<vector<int>> dp(m,vector<int>(n,-1));
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        dp[0][0]=grid[0][0];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(i==0&&j==0){dp[i][j]=grid[0][0];continue;}
-                int up=1e9;
-                int left=1e9;
-                if(i>0){up=dp[i-1][j]+grid[i][j];}
-                if(j>0){left=dp[i][j-1]+grid[i][j];}
-                dp[i][j]=min(up,left);
+                if(i==0&&j==0){continue;}
+                int lt=INT_MAX,up=INT_MAX;
+                if(i>0){up=dp[i-1][j];}
+                if(j>0){lt=dp[i][j-1];}
+                dp[i][j]=grid[i][j]+min(up,lt);
             }
         }
         return dp[m-1][n-1];
-    }
-};*/
-// space optimization
-class Solution {
-public:
-    int minPathSum(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        vector<int> prev(n,0);
-        for(int i=0;i<m;i++){
-            vector<int> curr(n,0);
-            for(int j=0;j<n;j++){
-                if(i==0&&j==0){curr[j]=grid[0][0];continue;}
-                int up=1e9;
-                int left=1e9;
-                if(i>0){up=prev[j]+grid[i][j];}
-                if(j>0){left=curr[j-1]+grid[i][j];}
-                curr[j]=min(up,left);
-            }
-            prev=curr;
-        }
-        return prev[n-1];
+        
     }
 };
