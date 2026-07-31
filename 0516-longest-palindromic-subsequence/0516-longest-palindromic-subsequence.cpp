bbf8a17e7a9e9ba8e36e71369fe0d86b63122839
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-07-31 11:33:04 +0530

    Time: 108 ms (5.41%), Space: 76 MB (22.55%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,41 +1,28 @@
 class Solution {
 public:
-    int longestPalindromeSubseq(string s) {
-        string t=s;
-        reverse(s.begin(),s.end());
+    int longestPalindromeSubseq(string s){
         int n=s.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
-        for(int i=0;i<=n;i++){
-            dp[i][0]=0;
-            dp[0][i]=0;
+        string t=s;
+        reverse(t.begin(),t.end());
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        //dp[i][j]=length of longest common subsequence in string s[0...i] and string t[0...j];
+        //dp[n-1][n-1];
+        for(int j=0;j<n;j++){
+            if(s[0]==t[j]){dp[0][j]=1;}
+            else if(j>0){dp[0][j]=dp[0][j-1];}
         }
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                int nottake=max(dp[i-1][j],dp[i][j-1]);
-                int take=0;
-                if(s[i-1]==t[j-1]){
-                    take=1+dp[i-1][j-1];
-                }
-                dp[i][j]=max(take,nottake);
+        for(int j=0;j<n;j++){
+            if(s[j]==t[0]){dp[j][0]=1;}
+            else if(j>0){dp[j][0]=dp[j-1][0];}
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<n;j++){
+                int tk=0;
+                if(s[i]==t[j]){tk=dp[i-1][j-1]+1;}
+                int nt=max(dp[i][j-1],dp[i-1][j]);
+                dp[i][j]=max(tk,nt);
             }
         }
-        int j=n,i=n;
-        string ans="";
-        while(i>0&&j>0){
-            if(s[i-1]==t[j-1]){
-                ans.push_back(s[i-1]);
-                i--;
-                j--;
-            }
-            else{
-                if(dp[i][j-1]>dp[i-1][j]){
-                    j--;
-                }
-                else{
-                    i--;
-                }
-            }
-        }
-        return dp[n][n];
+        return dp[n-1][n-1];
     }
 };
