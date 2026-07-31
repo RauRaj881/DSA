5a1cc6b49b061ecacbc2b76d6131fbb106e08224
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-07-31 10:13:22 +0530

    Time: 0 ms (100%), Space: 15.3 MB (54.59%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
-    int findTargetSumWays(vector<int>& nums, int tar){
+    int findTargetSumWays(vector<int>& nums,int tar){
         int n=nums.size();
-        int sm=0;
-        for(auto it:nums){sm+=it;}
-        if((sm+tar)%2!=0||abs(tar)>sm){return 0;}
-        int tar2=(sm+tar)/2;
-        vector<vector<int>> dp(n,vector<int>(tar2+1,0));
-        for(int i=0;i<n;i++){
-            dp[i][0]=1;
-        }
-        if(nums[0]<=tar2){dp[0][nums[0]]++;}
+        int sum=0;
+        for(auto it:nums){sum+=it;}
+        if((tar+sum)%2!=0||abs(tar)>sum){return 0;}
+        int trg=(tar+sum)/2;
+        vector<vector<int>> dp(n,vector<int>(trg+1,0));
+        dp[0][0]=1;
+        if(nums[0]<=trg){dp[0][nums[0]]++;}
         for(int i=1;i<n;i++){
-            for(int j=0;j<=tar2;j++){
+            for(int j=0;j<=trg;j++){
                 int nt=dp[i-1][j];
                 int t=0;
-                if(nums[i]<=j){
-                    t=dp[i-1][j-nums[i]];
-                }
-                dp[i][j]=t+nt;
+                if(nums[i]<=j){t=dp[i-1][j-nums[i]];}
+                dp[i][j]=nt+t;
             }
         }
-        return dp[n-1][tar2];
+        return dp[n-1][trg];
     }
 };
