0bb92b0096f2b8380726be75b9b015e90c58a27a
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-07-31 10:16:43 +0530

    Time: 7 ms (65.94%), Space: 15 MB (59.99%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -6,17 +6,19 @@ public:
         for(auto it:nums){sum+=it;}
         if((tar+sum)%2!=0||abs(tar)>sum){return 0;}
         int trg=(tar+sum)/2;
-        vector<vector<int>> dp(n,vector<int>(trg+1,0));
-        dp[0][0]=1;
-        if(nums[0]<=trg){dp[0][nums[0]]++;}
+        vector<int> prv(trg+1,0);
+        prv[0]=1;
+        if(nums[0]<=trg){prv[nums[0]]++;}
         for(int i=1;i<n;i++){
+            vector<int> cur(trg+1,0);
             for(int j=0;j<=trg;j++){
-                int nt=dp[i-1][j];
+                int nt=prv[j];
                 int t=0;
-                if(nums[i]<=j){t=dp[i-1][j-nums[i]];}
-                dp[i][j]=nt+t;
+                if(nums[i]<=j){t=prv[j-nums[i]];}
+                cur[j]=nt+t;
             }
+            prv=cur;
         }
-        return dp[n-1][trg];
+        return prv[trg];
     }
 };
