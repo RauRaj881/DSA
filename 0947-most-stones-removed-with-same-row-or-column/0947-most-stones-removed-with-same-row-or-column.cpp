c4b188c14c3dfb2b6a674c5357060da1d4c42176
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-08-11 02:25:17 +0530

    Time: 11 ms (82.24%), Space: 22.6 MB (47.05%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,47 +1,46 @@
 class Solution {
 public:
-int findUpar(int node,vector<int> & parent){
-    if(parent[node]==node){return node;}
-    return findUpar(parent[node],parent);
+int find_up(int u,vector<int>&parent){
+    if(parent[u]==u){return u;}
+    return find_up(parent[u],parent);
 }
-void disjoint(int u,int v,vector<int> & parent,vector<int> &rank){
-    int ulp_u=findUpar(u,parent);
-    int ulp_v=findUpar(v,parent);
-    if(ulp_u==ulp_v){return;}
-    if(rank[ulp_u]<rank[ulp_v]){
+void union_by_rank(int u,int v,vector<int>&parent,vector<int>&rank){{
+    int ulp_u=find_up(u,parent);
+    int ulp_v=find_up(v,parent);
+    if(rank[ulp_v]>rank[ulp_u]){
         parent[ulp_u]=ulp_v;
     }
-    else if(rank[ulp_u]>rank[ulp_v]){
+    else if(rank[ulp_v]<rank[ulp_u]){
         parent[ulp_v]=ulp_u;
     }
-    else if(rank[ulp_u]==rank[ulp_v]){
-        parent[ulp_u]=ulp_v;
-        rank[ulp_v]++;
+    else{
+        parent[ulp_v]=ulp_u;
+        rank[ulp_u]++;
     }
 }
-    int removeStones(vector<vector<int>>& stones) {
-        int maxrow=0,maxcol=0;
-        unordered_map<int,int> mp;
-        for(auto it:stones){
-            int row=it[0];
-            int col=it[1];
-            maxrow=max(maxrow,row);
-            maxcol=max(maxcol,col);
+
+}
+    int removeStones(vector<vector<int>>& s){
+        int n=s.size();
+        int mxr=0,mxc=0;
+        for(int i=0;i<n;i++){
+            int r=s[i][0],c=s[i][1];
+            mxr=max(mxr,r);mxc=max(mxc,c);
         }
-        vector<int> parent(maxrow+2+maxcol);
-        vector<int> rank(maxrow+2+maxcol,0);
-        for(int i=0;i<=maxrow+1+maxcol;i++){
+        vector<int> parent(mxr+mxc+2);
+        vector<int> rank(mxr+mxc+2,1);
+        for(int i=0;i<mxr+mxc+2;i++){
             parent[i]=i;
         }
-        for(int i=0;i<stones.size();i++){
-            disjoint(stones[i][0],stones[i][1]+maxrow+1,parent,rank);
-            mp[stones[i][0]]=1;
-            mp[stones[i][1]+maxrow+1]=1;
+        unordered_set<int> st;
+        for(int i=0;i<n;i++){
+            int u=s[i][0];
+            int v=s[i][1]+mxr+1;
+            union_by_rank(u,v,parent,rank);
+            st.insert(u);st.insert(v);
         }
-        int x=0;
-        for(auto it:mp){
-            if(parent[it.first]==it.first){x++;}
-        }
-        return stones.size()-x;
+        int comp=0;
+        for(auto it:st){if(parent[it]==it){comp++;}}
+        return n-comp;
     }
 };
