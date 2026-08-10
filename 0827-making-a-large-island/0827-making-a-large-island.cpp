22b147b7b1342a64d1974d9316e4df20db8c9c33
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-08-10 22:03:45 +0530

    Time: 285 ms (33.69%), Space: 159.3 MB (41.88%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.cpp
+++ 0827-making-a-large-island/0827-making-a-large-island.cpp
@@ -1,82 +1,63 @@
 class Solution {
 public:
-int findupar(int node,vector<int> & parent){
-    if(parent[node]==node){return node;}
-    return findupar(parent[node],parent);
+int find_up(int u,vector<int>&parent){
+    if(parent[u]==u){return u;}
+    return find_up(parent[u],parent);
 }
-void disjoint(int u,int v,vector<int> & parent,vector<int> &size){
-    int ulp_u=findupar(u,parent);
-    int ulp_v=findupar(v,parent);
-    if(ulp_u==ulp_v){return;}
+void union_by_size(int u,int v,vector<int>&parent,vector<int>&size){
+    int ulp_u=find_up(u,parent);
+    int ulp_v=find_up(v,parent);
     if(size[ulp_u]<size[ulp_v]){
         parent[ulp_u]=ulp_v;
         size[ulp_v]+=size[ulp_u];
     }
-    else {
+    else{
         parent[ulp_v]=ulp_u;
         size[ulp_u]+=size[ulp_v];
     }
 }
-    int largestIsland(vector<vector<int>>& grid) {
+    int largestIsland(vector<vector<int>>& grid){
         int n=grid.size();
         vector<int> parent(n*n);
-        vector<int> size(n*n,1);
         for(int i=0;i<n*n;i++){
             parent[i]=i;
         }
-        int drow[4]={-1,0,1,0};
-        int dcol[4]={0,1,0,-1};
-        for(int row=0;row<n;row++){
-            for(int col=0;col<n;col++){
-            int node =row*n+col;
-            if(grid[row][col]==1){
-            for(int i=0;i<4;i++){
-                int nexr=row+drow[i];
-                 int nexc=col+dcol[i];
-                 int adjnod=nexr*n+nexc;
-                 if(nexr>=0 && nexr<n && nexc>=0 && nexc<n){
-                 if(grid[nexr][nexc]==1){
-                 if(findupar(node,parent)!=findupar(adjnod,parent)){
-                    disjoint(node,adjnod,parent,size);
-                 }
-                 }
-            }
-            }
-            }
-            }
-        }
-        int maxno=0;
-        for(int checkr=0;checkr<n;checkr++){
-            for(int checkc=0;checkc<n;checkc++){
-                int checknod=checkr*n+checkc;
-            if(grid[checkr][checkc]==0){
-                set<int> components;
-            for(int i=0;i<4;i++){
-                int nexxr=checkr+drow[i];
-                 int nexxc=checkc+dcol[i];
-                 int checkadjnod=nexxr*n+nexxc;
-                 if(nexxr>=0 && nexxr<n && nexxc>=0 && nexxc<n){
-                 if(grid[nexxr][nexxc]==1){
-                    if(findupar(checknod,parent)!=findupar(checkadjnod,parent)){
-                        int pofnod=findupar(checkadjnod,parent);
-                        components.insert(pofnod);
+        vector<int> size(n*n,1);
+        int dx[4]={-1,0,1,0};
+        int dy[4]={0,1,0,-1};
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                int p=i*n+j;
+                if(grid[i][j]==1){
+                    for(int k=0;k<4;k++){
+                        int ni=i+dx[k],nj=j+dy[k];
+                        int np=ni*n+nj;
+                        if(ni<0||ni>=n||nj<0||nj>=n||grid[ni][nj]==0){continue;}
+                        if(find_up(p,parent)!=find_up(np,parent)){
+                            union_by_size(p,np,parent,size);
+                        }
                     }
-                 }
-        }
-            }
-            int totalsize=1;
-            for(auto it:components){
-                totalsize+=size[it];
-            }
-            maxno=max(maxno,totalsize);
-            }
+                }
             }
         }
-        for(int i=0;i<n*n;i++){
-    maxno = max(maxno, size[findupar(i, parent)]);
-}
-return maxno;
-
-        return maxno;
+        int mx=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                unordered_set<int> st;
+                if(grid[i][j]==0){
+                    for(int k=0;k<4;k++){
+                        int ni=i+dx[k],nj=j+dy[k];
+                        int np=ni*n+nj;
+                        if(ni<0||ni>=n||nj<0||nj>=n||grid[ni][nj]==0){continue;}
+                        st.insert(find_up(np,parent));
+                    }
+                int tot=1;
+                for(auto it:st){tot+=size[it];}
+                mx=max(mx,tot);
+                }
+            }
+        }
+        if(mx==0){mx=n*n;}
+        return mx;
     }
 };
