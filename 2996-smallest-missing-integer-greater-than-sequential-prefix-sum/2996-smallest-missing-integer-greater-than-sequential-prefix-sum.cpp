bac2a8513fb4ff63f094d40f0df1ffabf3bc4032
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-08-12 02:02:58 +0530

    Time: 0 ms (100%), Space: 23.1 MB (6.55%) - LeetHub

--- 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
@@ -9,7 +9,7 @@ public:
             if(nums[i]==nums[i-1]+1&&pos){sm+=nums[i];st.insert(nums[i]);}
             else{pos=false;st.insert(nums[i]);}
         }
-        for(int i=sm;i<=2000;i++){
+        for(int i=sm;i<=1275;i++){
             if(!st.count(i)){return i;}
         }
         return -1;
