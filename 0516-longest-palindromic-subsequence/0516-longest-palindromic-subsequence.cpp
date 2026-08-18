bac56a97c516b1b16c1a510808784600ae214235
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-08-18 15:46:49 +0530

    Time: 124 ms (5.01%), Space: 76.1 MB (8.22%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -4,25 +4,17 @@ public:
         int n=s.size();
         string t=s;
         reverse(t.begin(),t.end());
-        vector<vector<int>> dp(n,vector<int>(n,0));
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
         //dp[i][j]=length of longest common subsequence in string s[0...i] and string t[0...j];
         //dp[n-1][n-1];
-        for(int j=0;j<n;j++){
-            if(s[0]==t[j]){dp[0][j]=1;}
-            else if(j>0){dp[0][j]=dp[0][j-1];}
-        }
-        for(int j=0;j<n;j++){
-            if(s[j]==t[0]){dp[j][0]=1;}
-            else if(j>0){dp[j][0]=dp[j-1][0];}
-        }
-        for(int i=1;i<n;i++){
-            for(int j=1;j<n;j++){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
                 int tk=0;
-                if(s[i]==t[j]){tk=dp[i-1][j-1]+1;}
+                if(s[i-1]==t[j-1]){tk=dp[i-1][j-1]+1;}
                 int nt=max(dp[i][j-1],dp[i-1][j]);
                 dp[i][j]=max(tk,nt);
             }
         }
-        return dp[n-1][n-1];
+        return dp[n][n];
     }
 };
