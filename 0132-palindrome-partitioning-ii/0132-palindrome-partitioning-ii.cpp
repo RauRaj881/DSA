2dc158ec73bef8ec6feb651b713f0af62ac3564a
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-08-25 23:45:30 +0530

    Time: 26 ms (94.94%), Space: 12 MB (43.27%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
-bool ispalin(int j,int i,string &s){
-    while(j<i){
-        if(s[j]!=s[i]){return false;}
-        j++;i--;
-    }
-    return true;
-}
     int minCut(string s){
         int n=s.size();
         vector<int> dp(n,n+1);
+        vector<vector<bool>> palin(n,vector<bool>(n,false));
         dp[0]=1;
         for(int i=1;i<n;i++){
             for(int j=0;j<=i;j++){
-                if(ispalin(j,i,s)){
-                    int x=(j==0?0:dp[j-1]);
-                    dp[i]=min(dp[i],x+1);
+                if(s[i]==s[j]&&(i-j<=2||palin[j+1][i-1])){
+                    palin[j][i]=true;
+                    dp[i]=min(dp[i],(j==0?1:dp[j-1]+1));
                 }
             }
         }
