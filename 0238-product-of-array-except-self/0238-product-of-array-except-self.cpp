a42e2ed4cee6ef2fc538479835de553285fb738a
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-08-27 00:58:38 +0530

    Time: 0 ms (100%), Space: 40 MB (95.94%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,31 +1,10 @@
-/*class Solution {
-public:
-    vector<int> productExceptSelf(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> preprod(n);
-        vector<int> sufprod(n);
-        int prod1=1,prod2=1;
-        for(int i=0;i<n;i++){
-            prod1=prod1*nums[i];
-            prod2=prod2*nums[n-i-1];
-            preprod[i]=prod1;
-            sufprod[n-i-1]=prod2;
-        }
-        for(int i=0;i<n;i++){
-            if(i==0){nums[i]=sufprod[1];}
-            else if(i==n-1){nums[i]=preprod[n-2];}
-            else{nums[i]=preprod[i-1]*sufprod[i+1];}
-        }
-        return nums;
-    }
-};*/
-//O(1) space
 class Solution {
 public:
-    vector<int> productExceptSelf(vector<int>& nums) {
+    vector<int> productExceptSelf(vector<int>& nums){
         int n=nums.size();
-        int prod1=1,prod2=1;
         vector<int> ans(n);
+        int prod1=1;
+        int prod2=1;
         for(int i=0;i<n;i++){
             ans[i]=prod1;
             prod1*=nums[i];
