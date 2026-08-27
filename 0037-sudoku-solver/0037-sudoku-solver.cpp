8f58a99b0b86b45b806e8df3d16cd4bb868b6d8f
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-08-27 17:41:04 +0530

    Time: 484 ms (14.62%), Space: 8.7 MB (79.77%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,38 +1,36 @@
 class Solution {
 public:
-bool isvalid(int row,int col,vector<vector<char>>& board,int k){
+bool isvalid(int row,int col,int k,vector<vector<char>>& b){
     for(int i=0;i<9;i++){
-        if(board[i][col]==char(k+'0')){
+        if(b[i][col]==char(k+'0')){
             return false;
         }
-        if(board[row][i]==char(k+'0')){
+        if(b[row][i]==char(k+'0')){
             return false;
         }
-        if(board[3*(row/3)+i/3][3*(col/3)+i%3]==char(k+'0')){
+        if(b[3*(row/3)+i/3][3*(col/3)+i%3]==char(k+'0')){
             return false;
         }
     }
     return true;
 }
-bool sudoku(vector<vector<char>>& board){
-    for(int i=0;i<9;i++){
+bool finished=false;
+    void solveSudoku(vector<vector<char>>& b){
+        for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
-                if(board[i][j]=='.'){
-                    for(int k=1;k<=9;k++){
-                        if(isvalid(i,j,board,k)){
-                            board[i][j]=char(k+'0');
-                            if(sudoku(board)==true){return true;}
-                            board[i][j]='.';
-                            }
+                if(b[i][j]!='.'){continue;}
+                for(int k=1;k<=9;k++){
+                    if(isvalid(i,j,k,b)){
+                        b[i][j]=k+'0';
+                        solveSudoku(b);
+                        if(finished){return;}
                     }
-                    return false;
+                    b[i][j]='.';
                 }
+                return;
             }
         }
-        return true;
-}
-    void solveSudoku(vector<vector<char>>& board) {
-        sudoku(board);
-        //return board;
+        finished=true;
+        
     }
 };
