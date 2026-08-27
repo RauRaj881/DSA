d661033f0db57c85aeadfd37dc0e91acc88f10df
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-08-27 18:53:37 +0530

    Time: 3 ms (25.83%), Space: 10.9 MB (35.3%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
 vector<vector<int>> ans;
-unordered_set<int> st;
-void f(vector<int>& nums,vector<int>& path){
-    if(path.size()==nums.size()){ans.push_back(path);return;}
-    for(int i=0;i<nums.size();i++){
-        if(!st.count(nums[i])){
-            st.insert(nums[i]);
-            path.push_back(nums[i]);
-            f(nums,path);
-            path.pop_back();
-            st.erase(nums[i]);
-        }
+void f(int i,int n,vector<int>&nums){
+    if(i==n){ans.push_back(nums);return;}
+    for(int idx=i;idx<n;idx++){
+        swap(nums[i],nums[idx]);
+        f(i+1,n,nums);
+        swap(nums[i],nums[idx]);
     }
 }
     vector<vector<int>> permute(vector<int>& nums){
-        vector<int> path;
-        f(nums,path);
+        int n=nums.size();
+        f(0,n,nums);
         return ans;
     }
 };
