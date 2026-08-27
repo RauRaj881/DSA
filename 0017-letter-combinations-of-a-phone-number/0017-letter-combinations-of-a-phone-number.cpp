8d6303a64e3c3b398412f4c69fa6199dd12e7017
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-08-27 18:29:53 +0530

    Time: 0 ms (100%), Space: 9.3 MB (44.05%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-void f(vector<string> &ans,string &digits,vector<string> &phone,string &s,int i){
-    if(i==digits.size()){ans.push_back(s);return;}
-    int x=digits[i]-'0';
-    for(int j=0;j<phone[x].size();j++){
-        string curr=phone[x];
-        s+=curr[j];
-        f(ans,digits,phone,s,i+1);
+vector<string> ans;
+void f(int i,vector<string>& phone,int n,string& s,string &d){
+    if(i==n){ans.push_back(s);return;}
+    for(int j=0;j<phone[d[i]-'0'].size();j++){
+        s+=phone[d[i]-'0'][j];
+        f(i+1,phone,n,s,d);
         s.pop_back();
     }
 }
-    vector<string> letterCombinations(string digits) {
-        vector<string> ans;
-        if(digits.size()==0){return ans;}
-        string s;
+
+    vector<string> letterCombinations(string d){
+        int n=d.size();
+        string s="";
         vector<string> phone={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        f(ans,digits,phone,s,0);
+        f(0,phone,n,s,d);
         return ans;
     }
 };
