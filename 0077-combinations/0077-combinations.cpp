cae3ce7f2ead84447f2bcc6113b42363de274a61
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-08-27 18:34:06 +0530

    Time: 109 ms (13.13%), Space: 132.9 MB (8.32%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-void f(int i,int n,int k,vector<int> &path,vector<vector<int>> &ans){
-    if(i>n){if(path.size()==k){ans.push_back(path);}return;}
-    path.push_back(i);
-    f(i+1,n,k,path,ans);
-    path.pop_back();
-    f(i+1,n,k,path,ans);
+vector<vector<int>> ans;
+void f(int i,int n,int k,vector<int>&v){
+    if(i>n){if(v.size()==k){ans.push_back(v);}return;}
+    v.push_back(i);
+    f(i+1,n,k,v);
+    v.pop_back();
+    f(i+1,n,k,v);
 }
     vector<vector<int>> combine(int n, int k){
-        vector<vector<int>> ans;
-        vector<int> path;
-        f(1,n,k,path,ans);
+        vector<int> v;
+        f(1,n,k,v);
         return ans;
     }
 };
