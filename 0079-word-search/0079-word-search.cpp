027add56c5d65f8e2a735109b3cf5d321fc89378
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-08-28 17:11:18 +0530

    Time: 499 ms (34.38%), Space: 12.3 MB (5.63%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,40 +1,30 @@
 class Solution {
 public:
-bool doexist(int row,int col,vector<vector<char>>& board,string word,int index,int m,int n,vector<vector<int>> &vis,int drow[],int dcol[]){
-    if(index==word.size()){
-        return true;
+bool pos=false;
+int dx[4]={-1,0,1,0};
+int dy[4]={0,1,0,-1};
+void f(int row,int col,vector<vector<char>>& b,string w,int i,vector<vector<int>>& vis){
+    int n=b.size(),m=b[0].size();
+    if(b[row][col]!=w[i]){return;}
+    if(i==w.size()-1){pos=true;return;}
+    vis[row][col]=1;
+    for(int k=0;k<4;k++){
+        int nr=row+dx[k];
+        int nc=col+dy[k];
+        if(nr<0||nr>=n||nc<0||nc>=m||vis[nr][nc]==1){continue;}
+        f(nr,nc,b,w,i+1,vis);
     }
-    if(row<0 ||row>=m ||col<0 ||col>=n ||word[index]!=board[row][col] ||vis[row][col]==1){
-        return false;
-    }
-        vis[row][col]=1;
-        for(int i=0;i<4;i++){
-            int nrow=row+drow[i];
-            int ncol=col+dcol[i];
-            if(nrow>=0 && nrow<m && ncol>=0 && ncol<n){
-
-                if(doexist(nrow,ncol,board,word,index+1,m,n,vis,drow,dcol)){
-                    return true;
-                }
+    vis[row][col]=-1;
+}
+    bool exist(vector<vector<char>>& b,string w){
+        int n=b.size();
+        int m=b[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                vector<vector<int>> vis(n,vector<int>(m,-1));
+                f(i,j,b,w,0,vis);
             }
         }
-        vis[row][col]=0;
-        return false;
-        }
-    bool exist(vector<vector<char>>& board, string word) {
-        int drow[4]={-1,0,1,0};
-        int dcol[4]={0,1,0,-1}; 
-        int m=board.size();
-        int n=board[0].size();
-        if(m==1 && n==1 && word.size()==1 && board[0][0]==word[0]){return true;}
-        vector<vector<int>> vis(m,vector<int>(n,0));
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(doexist(i,j,board,word,0,m,n,vis,drow,dcol)){
-                    return true;
-                }
-            }
-        }
-        return false;     
+        return pos;
     }
 };
