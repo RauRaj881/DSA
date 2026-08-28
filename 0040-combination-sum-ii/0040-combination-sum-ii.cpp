f34e2188e638a225713e732de7efd5ffedf068e3
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-08-28 13:15:31 +0530

    Time: 1 ms (50.1%), Space: 14.3 MB (20.35%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    void backtrack(vector<vector<int>>& v, vector<int>& ans,
-vector<int>& c, int target, int sum, int idx){
-    if(sum==target){v.push_back(ans);return;}
-    for(int i=idx;i<c.size();i++){
-        if(i>idx&&c[i]==c[i-1]){continue;}
-        if(sum+c[i]>target){break;}
-        ans.push_back(c[i]);
-        backtrack(v,ans,c,target,sum+c[i],i+1);
-        ans.pop_back();
+vector<vector<int>> ans;
+void f(int i,int sm,vector<int>& c, int tar,vector<int>& tp){
+    if(sm==tar){ans.push_back(tp);return;}
+    if(sm>tar){return;}
+    for(int idx=i;idx<c.size();idx++){
+        if(idx>i&&c[idx]==c[idx-1]){continue;}
+        tp.push_back(c[idx]);
+        f(idx+1,sm+c[idx],c,tar,tp);
+        tp.pop_back();
     }
-
-    }
-    vector<vector<int>> combinationSum2(vector<int>& c, int target) {
-        sort(c.begin(), c.end());
-        vector<vector<int>> v;
-        vector<int> ans;
-        backtrack(v, ans, c, target, 0, 0);
-        return v;
+}
+    vector<vector<int>> combinationSum2(vector<int>& c, int tar){
+        sort(c.begin(),c.end());
+        vector<int> tp;
+        f(0,0,c,tar,tp);
+        return ans;
     }
 };
