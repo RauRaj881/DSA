400d5f371fe8dcb9a61dabc219cbee46237736f7
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-08-28 15:25:46 +0530

    Time: 0 ms (100%), Space: 8.2 MB (56.05%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+int ans=0;
+bool issafe(int row,int col,vector<string>& board,int n){
+    for(int i=0;i<n;i++){
+        if(board[i][col]=='Q'){return false;}
+    }
+    for(int j=0;j<n;j++){
+        if(board[row][j]=='Q'){return false;}
+    }
+    int i=row,j=col;
+    while(i>=0&&j>=0){
+        if(board[i][j]=='Q'){return false;}
+        i--;j--;
+    }
+    i=row,j=col;
+    while(i>=0&&j<n){
+        if(board[i][j]=='Q'){return false;}
+        i--;j++;
+    }
+    return true;
+}
+void f(int row,int n,vector<string>&board){
+    if(row==n){ans++;return;}
+    for(int col=0;col<n;col++){
+        if(issafe(row,col,board,n)){
+            board[row][col]='Q';
+            f(row+1,n,board);
+            board[row][col]='.';
+        }
+    }
+}
+    int totalNQueens(int n){
+        vector<string> board(n);
+        for(int i=0;i<n;i++){
+            board[i]=string(n,'.');
+        }
+        f(0,n,board);
+        return ans;
+    }
+};
