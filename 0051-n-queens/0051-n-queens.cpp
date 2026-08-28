18747ce4975602200e0513e501707c99266edf3b
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-08-28 15:19:44 +0530

    Time: 6 ms (18.72%), Space: 10.5 MB (32.67%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,54 +1,41 @@
 class Solution {
 public:
-bool issafe(int row,int col,vector<string> &board){
-    int orirow=row;
-    int oricol=col;
-    for(int j=0;j<board.size();j++){
-        if(board[row][j]=='Q'){
-            return false;
-        }
+vector<vector<string>> ans;
+bool issafe(int row,int col,vector<string>& board,int n){
+    for(int i=0;i<n;i++){
+        if(board[i][col]=='Q'){return false;}
     }
-    row=orirow;
-    col=oricol;
-    while(row>=0 && col>=0){
-        if(board[row][col]=='Q'){
-            return false;
-        }
-        row--;col--;
+    for(int j=0;j<n;j++){
+        if(board[row][j]=='Q'){return false;}
     }
-    row=orirow;
-    col=oricol;
-    while(row<board.size() && col>=0){
-        if(board[row][col]=='Q'){
-            return false;
-        }
-        row++;
-        col--;
+    int i=row,j=col;
+    while(i>=0&&j>=0){
+        if(board[i][j]=='Q'){return false;}
+        i--;j--;
+    }
+    i=row,j=col;
+    while(i>=0&&j<n){
+        if(board[i][j]=='Q'){return false;}
+        i--;j++;
     }
     return true;
 }
-void solve(int col,vector<vector<string>> &ans,vector<string> &board,int n){
-    if(col==n){
-        ans.push_back(board);
-        return;
-    }
-    for(int row=0;row<n;row++){
-        if(issafe(row,col,board)){
+void f(int row,int n,vector<string>& board){
+    if(row==n){ans.push_back(board);return;}
+    for(int col=0;col<n;col++){
+        if(issafe(row,col,board,n)){
             board[row][col]='Q';
-            solve(col+1,ans,board,n);
+            f(row+1,n,board);
             board[row][col]='.';
         }
     }
 }
-    vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> ans;
+    vector<vector<string>> solveNQueens(int n){
         vector<string> board(n);
-        string s(n,'.');
         for(int i=0;i<n;i++){
-            board[i]=s;
+            board[i]=string(n,'.');
         }
-        //vector<string> board(n, string(n, '.'));
-        solve(0,ans,board,n);
+        f(0,n,board);
         return ans;
     }
 };
