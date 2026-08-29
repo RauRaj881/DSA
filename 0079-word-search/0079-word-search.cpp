ea9207af4f765608170b4151300472e3c348144c
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-08-29 10:49:01 +0530

    Time: 470 ms (35.6%), Space: 11.5 MB (42.81%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -3,26 +3,26 @@ public:
 bool pos=false;
 int dx[4]={-1,0,1,0};
 int dy[4]={0,1,0,-1};
-void f(int row,int col,vector<vector<char>>& b,string w,int i,vector<vector<int>>& vis){
+void f(int row,int col,vector<vector<char>>& b,string w,int i){
     int n=b.size(),m=b[0].size();
     if(b[row][col]!=w[i]){return;}
     if(i==w.size()-1){pos=true;return;}
-    vis[row][col]=1;
+    char ch=b[row][col];
+    b[row][col]='#';
     for(int k=0;k<4;k++){
         int nr=row+dx[k];
         int nc=col+dy[k];
-        if(nr<0||nr>=n||nc<0||nc>=m||vis[nr][nc]==1){continue;}
-        f(nr,nc,b,w,i+1,vis);
+        if(nr<0||nr>=n||nc<0||nc>=m||b[nr][nc]=='#'){continue;}
+        f(nr,nc,b,w,i+1);
     }
-    vis[row][col]=-1;
+    b[row][col]=ch;
 }
     bool exist(vector<vector<char>>& b,string w){
         int n=b.size();
         int m=b[0].size();
-        vector<vector<int>> vis(n,vector<int>(m,-1));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                f(i,j,b,w,0,vis);
+                f(i,j,b,w,0);
             }
         }
         return pos;
