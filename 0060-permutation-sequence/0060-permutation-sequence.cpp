aaeb082c62ad590f4ec5cbbdf65d27ae7ca07fe4
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-09-15 22:37:46 +0530

    Time: 0 ms (100%), Space: 8.1 MB (78.17%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-int fact(int i){
-    int ans=1;
-    for(int k=1;k<=i;k++){
-        ans*=k;
-    }
-    return ans;
-}
     string getPermutation(int n, int k){
         string ans="";
         unordered_set<int> st;
+        vector<int> fact(n+1,1);
+        int cr=1;
+        for(int i=1;i<=n;i++){
+            cr*=i;
+            fact[i]=cr;
+        }
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
                 if(st.count(j)){continue;}
-                int af=fact(n-i);
+                int af=fact[n-i];
                 if(af<k){k-=af;}
                 else{ans+=j+'0';st.insert(j);break;}
             }
