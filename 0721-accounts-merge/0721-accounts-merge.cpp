3119ff1a7599dab5d300e6b35de25035b172ceb1
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-09-15 15:03:08 +0530

    Time: 31 ms (62.06%), Space: 40.5 MB (47.39%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.cpp
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-int find_up(int u,vector<int>& parent){
+int findup(int u,vector<int>&parent){
     if(parent[u]==u){return u;}
-    return find_up(parent[u],parent);
+    return findup(parent[u],parent);
 }
-void union_by_rank(int u,int v,vector<int>& parent,vector<int>& rank){
-    int ulp_u=find_up(u,parent);
-    int ulp_v=find_up(v,parent);
+void union_by_rank(int u,int v,vector<int>&parent,vector<int>&rank){
+    int ulp_u=findup(u,parent);
+    int ulp_v=findup(v,parent);
     if(rank[ulp_u]>rank[ulp_v]){
         parent[ulp_v]=ulp_u;
     }
@@ -18,38 +18,36 @@ void union_by_rank(int u,int v,vector<int>& parent,vector<int>& rank){
         rank[ulp_u]++;
     }
 }
-    vector<vector<string>> accountsMerge(vector<vector<string>>& acc){
-        int n=acc.size();
-        unordered_map<string,int> mp;
+    vector<vector<string>> accountsMerge(vector<vector<string>>& acn){
+        int n=acn.size();
         vector<int> parent(n);
-        vector<int> rank(n,1);
         for(int i=0;i<n;i++){parent[i]=i;}
+        vector<int> rank(n,1);
+        unordered_map<string,int> mp;
         for(int i=0;i<n;i++){
-            for(int j=1;j<acc[i].size();j++){
-                string gmail=acc[i][j];
-                if(mp.count(gmail)){
-                    union_by_rank(i,mp[gmail],parent,rank);
+            for(int j=1;j<acn[i].size();j++){
+                if(mp.count(acn[i][j])){
+                    union_by_rank(i,mp[acn[i][j]],parent,rank);
                 }
                 else{
-                    mp[gmail]=i;
+                    mp[acn[i][j]]=i;
                 }
             }
         }
         vector<vector<string>> v(n);
         for(auto it:mp){
             string cr=it.first;
-            int u=it.second;
-            int ulp_u=find_up(u,parent);
-            v[ulp_u].push_back(cr);
+            int nd=findup(it.second,parent);
+            v[nd].push_back(cr);
         }
         vector<vector<string>> ans;
         for(int i=0;i<n;i++){
             if(v[i].empty()){continue;}
             vector<string> tp;
-            tp.push_back(acc[i][0]);
+            tp.push_back(acn[i][0]);
             sort(v[i].begin(),v[i].end());
-            for(auto it:v[i]){
-                tp.push_back(it);
+            for(int j=0;j<v[i].size();j++){
+                tp.push_back(v[i][j]);
             }
             ans.push_back(tp);
         }
