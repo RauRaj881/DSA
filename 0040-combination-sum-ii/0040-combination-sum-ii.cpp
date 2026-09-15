213ad78ca165f35bb38b80b27ce340541766bd4d
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-09-15 23:41:52 +0530

    Time: 1 ms (49.29%), Space: 14.3 MB (29.03%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
 vector<vector<int>> ans;
-void f(int i,int sm,vector<int>& c, int tar,vector<int>& tp){
+void f(int i,int n,vector<int>& c,int tar,vector<int>& tp,int sm){
     if(sm==tar){ans.push_back(tp);return;}
     if(sm>tar){return;}
-    for(int idx=i;idx<c.size();idx++){
+    for(int idx=i;idx<n;idx++){
         if(idx>i&&c[idx]==c[idx-1]){continue;}
         tp.push_back(c[idx]);
-        f(idx+1,sm+c[idx],c,tar,tp);
+        f(idx+1,n,c,tar,tp,sm+c[idx]);
         tp.pop_back();
     }
 }
-    vector<vector<int>> combinationSum2(vector<int>& c, int tar){
+
+    vector<vector<int>> combinationSum2(vector<int>& c,int tar){
         sort(c.begin(),c.end());
+        int n=c.size();
         vector<int> tp;
-        f(0,0,c,tar,tp);
+        f(0,n,c,tar,tp,0);
         return ans;
     }
 };
