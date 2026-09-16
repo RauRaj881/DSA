25e13eca869683a1543589dcc984da6d9aff4760
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-09-16 14:58:25 +0530

    Time: 20 ms (8.91%), Space: 21.1 MB (8.35%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
 vector<vector<int>> ans;
-void dfs(int nd,vector<vector<int>>& adj,vector<int>& tp,int n){
+void dfs(int nd,vector<vector<int>>& adj,vector<int> tp,int n){
     tp.push_back(nd);
     if(nd==n-1){ans.push_back(tp);return;}
     for(auto it:adj[nd]){
         dfs(it,adj,tp,n);
-        tp.pop_back();
     }
 }
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& g){
         int n=g.size();
-        vector<vector<int>> adj(n);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<g[i].size();j++){
-                adj[i].push_back(g[i][j]);
-            }
-        }
         vector<int> tp;
-        dfs(0,adj,tp,n);
+        dfs(0,g,tp,n);
         return ans;
     }
 };
