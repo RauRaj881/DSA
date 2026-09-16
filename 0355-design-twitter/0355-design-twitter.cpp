86f0835e7360a64bc21c20dfc613a8d181b6d28c
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-09-16 17:37:20 +0530

    Time: 11 ms (38.39%), Space: 58 MB (13.46%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.cpp
+++ 0355-design-twitter/0355-design-twitter.cpp
@@ -1,49 +1,49 @@
 class Twitter {
 public:
-unordered_map<int,unordered_set<int>> following;
-unordered_map<int,vector<pair<int,int>>> tweets;
-int time=0;
+unordered_map<int,unordered_set<int>> fl;
+unordered_map<int,vector<pair<int,int>>> twt;
+int tm=0;
     Twitter() {
     }
     
-    void postTweet(int userId, int tweetId){
-        tweets[userId].push_back({time,tweetId});
-        time++;
+    void postTweet(int uId,int tId){
+        twt[uId].push_back({tId,tm});
+        tm++;
     }
     
-    vector<int> getNewsFeed(int userId){
-        vector<int> ans;
+    vector<int> getNewsFeed(int uId){
         vector<int> users;
-        users.push_back(userId);
-        for(auto it:following[userId]){
+        users.push_back(uId);
+        for(auto it:fl[uId]){
             users.push_back(it);
         }
         priority_queue<vector<int>> pq;
         for(int i=0;i<users.size();i++){
-            //for(auto it:tweets[users[i]]){
-            vector<pair<int,int>> it=tweets[users[i]];
-                int sz=it.size();
-                if(sz>0)pq.push({it[sz-1].first,it[sz-1].second,users[i],sz-1});
-            //}
+            int sz=twt[users[i]].size();
+            vector<pair<int,int>> tp=twt[users[i]];
+            if(sz>0){
+                pq.push({tp[sz-1].second,tp[sz-1].first,users[i],sz-1});
+            }
         }
+        vector<int> ans;
         while(!pq.empty()&&ans.size()<10){
-            vector<int> cr=pq.top();
+            vector<int> tp=pq.top();
             pq.pop();
-            ans.push_back(cr[1]);
-            int idx=cr[3],u=cr[2];
+            int u=tp[2],idx=tp[3];
+            ans.push_back(tp[1]);
             if(idx-1>=0){
-                pq.push({tweets[u][idx-1].first,tweets[u][idx-1].second,u,idx-1});
+                pq.push({twt[u][idx-1].second,twt[u][idx-1].first,u,idx-1});
             }
         }
         return ans;
     }
     
     void follow(int followerId, int followeeId){
-        following[followerId].insert(followeeId);
+        fl[followerId].insert(followeeId);
     }
     
     void unfollow(int followerId, int followeeId){
-        following[followerId].erase(followeeId);
+        fl[followerId].erase(followeeId);
     }
 };
 
