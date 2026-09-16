e93a8f9944280af44dcd24a27396e9867ce35df4
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-09-16 20:54:59 +0530

    Time: 22 ms (5.25%), Space: 19.7 MB (5%) - LeetHub

--- 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.cpp
+++ 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.cpp
@@ -1,43 +1,31 @@
 class Solution {
 public:
-    bool parseBoolExpr(string exp) {
-        stack<char> st;
+    bool parseBoolExpr(string exp){
         int n=exp.size();
+        stack<char> st;
         for(int i=0;i<n;i++){
             if(exp[i]!=')'){
                 st.push(exp[i]);
             }
             else{
                 unordered_map<char,int> mp;
-                while(!(st.top()=='!'||st.top()=='&'||st.top()=='|')){
+                while(!(st.top()=='|'||st.top()=='&'||st.top()=='!')){
                     mp[st.top()]++;st.pop();
                 }
                 if(st.top()=='|'){
                     st.pop();
-                    if(mp.count('t')){
-                        st.push('t');
-                    }
-                    else{
-                        st.push('f');
-                    }
+                    if(mp.count('t')){st.push('t');}
+                    else{st.push('f');}
                 }
                 else if(st.top()=='&'){
                     st.pop();
-                    if(mp.count('f')){
-                        st.push('f');
-                    }
-                    else{
-                        st.push('t');
-                    }
+                    if(mp.count('f')){st.push('f');}
+                    else{st.push('t');}
                 }
                 else{
                     st.pop();
-                    if(mp.count('f')){
-                        st.push('t');
-                    }
-                    else{
-                        st.push('f');
-                    }
+                    if(mp.count('t')){st.push('f');}
+                    else{st.push('t');}
                 }
             }
         }
