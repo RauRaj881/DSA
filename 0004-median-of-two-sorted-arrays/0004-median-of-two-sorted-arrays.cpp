3308765997695c451680b4add58c097ac6938a66
Raushan Raj <218284691+RauRaj881@users.noreply.github.com>
2026-09-22 12:08:19 +0530

    Time: 0 ms (100%), Space: 95.2 MB (41.6%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        // Always binary search on the smaller array
-        if (nums2.size() > nums1.size())
-            return findMedianSortedArrays(nums2, nums1);
-        int n1 = nums1.size();
-        int n2 = nums2.size();
-        int total = n1 + n2;
-
-        int low = max(0, total / 2 - n2);
-        int high = min(n1, total / 2);
-
-        while (low <= high) {
-            int cut1 = (low + high) / 2;
-            int cut2 = total / 2 - cut1;
-
-            int left1  = (cut1 == 0) ? INT_MIN : nums1[cut1 - 1];
-            int right1 = (cut1 == n1) ? INT_MAX : nums1[cut1];
-
-            int left2  = (cut2 == 0) ? INT_MIN : nums2[cut2 - 1];
-            int right2 = (cut2 == n2) ? INT_MAX : nums2[cut2];
-
-            if (left1 > right2) {
-                high = cut1 - 1;
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2){
+        if(nums1.size()<nums2.size()){
+            return findMedianSortedArrays(nums2,nums1);
+        }
+        int n1=nums1.size();
+        int n2=nums2.size();
+        int tot=n1+n2;
+        int lthf=(n1+n2)/2;
+        int low=max(0,lthf-n2);
+        int high=min(n1,lthf);
+        while(low<=high){
+            int mid=(low+high)/2;
+            int rem=lthf-mid;
+            int mx1=(mid>0)?nums1[mid-1]:INT_MIN;
+            int mx2=(rem>0)?nums2[rem-1]:INT_MIN;
+            int mn1=(mid<n1)?nums1[mid]:INT_MAX;
+            int mn2=(rem<n2)?nums2[rem]:INT_MAX;
+            if(mx1>mn2){
+                high=mid-1;
             }
-            else if (left2 > right1) {
-                low = cut1 + 1;
+            else if(mx2>mn1){
+                low=mid+1;
             }
-            else {
-                if (total % 2 == 1)
-                    return min(right1, right2);
-
-                return (max(left1, left2) + min(right1, right2)) / 2.0;
+            else{
+                if(tot%2==1){
+                    return min(mn1,mn2);
+                }
+                else{
+                    return (max(mx1,mx2)+min(mn1,mn2))/2.0;
+                }
             }
         }
-
         return 0.0;
+        
     }
 };
